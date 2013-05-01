bf74a5fa5950767bd9ddedf2490539f7acde809b
Andrew Otto <acotto@gmail.com>
2013-05-01 11:10:26 -0400

    user_metrics module changes:
    - Fixing git clone
    - creating admin account
    - seeding MediaWiki db with data good for user_metrics testing.
    
    Change-Id: Iab806d80c2570adaf22cbfbb64c112c11e817c82

--- puppet/modules/user_metrics/manifests/init.pp
+++ puppet/modules/user_metrics/manifests/init.pp
@@ -41,24 +41,42 @@ class user_metrics {
 		ensure => 'installed',
 	}
 
-	git::clone { 'analytics/E3Analysis':
+	git::clone { 'analytics/user-metrics':
 		directory => $user_metrics_path,
 		require   => [Package['python-flask']],
 	}
 
 	# create the user_metrics cohorts database
 	exec { 'user_metrics_mysql_create_database':
-		command => "/usr/bin/mysql -pvagrant -e \"CREATE DATABASE ${user_metrics_db_name};\" &&/usr/bin/mysql -pvagrant ${user_metrics_db_name} < ${user_metrics_path}/scripts/user_metrics.sql;",
-		unless  => "/usr/bin/mysql -pvagrant -e 'SHOW DATABASES' | /bin/grep -q ${user_metrics_db_name}",
-		user    => 'root',
+		command   => "/usr/bin/mysql -pvagrant -e \"CREATE DATABASE ${user_metrics_db_name};\" && /usr/bin/mysql -pvagrant ${user_metrics_db_name} < ${user_metrics_path}/scripts/user_metrics.sql;",
+		unless    => "/usr/bin/mysql -pvagrant -e 'SHOW DATABASES' | /bin/grep -q ${user_metrics_db_name}",
+		user      => 'root',
 		logoutput => true,
-		require => [Git::Clone['analytics/E3Analysis'], Service['mysql']]
+		require   => [Git::Clone['analytics/user-metrics'], Service['mysql']]
 	}
 
 	# Need settings.py to configure metrics-api python application
 	file { "${user_metrics_path}/user_metrics/config/settings.py":
 		content => template('user_metrics/settings.py.erb'),
-		require => Git::Clone['analytics/E3Analysis'],
+		require => Git::Clone['analytics/user-metrics'],
+	}
+
+	# create default admin account
+	exec { 'user_metrics_create_admin_account':
+		command => "/usr/bin/python ${user_metrics_path}/scripts/create_account.py",
+		# Yes, this script loads via relative paths.  Sigh...
+		cwd     => "${user_metrics_path}/scripts",
+		unless  => '/usr/bin/mysql -pvagrant user_metrics -e "select \'exists\' from api_user where user_name = \'admin\'" | /bin/grep -q exists',
+		require => [Exec['user_metrics_mysql_create_database'], File["${user_metrics_path}/user_metrics/config/settings.py"]],
+	}
+
+	# Seed the MediaWiki wiki database with data good for testing user_metrics API.
+	exec { 'user_metrics_mysql_seed_mediawiki_database':
+		command   => "/usr/bin/mysql -f -pvagrant wiki < ${user_metrics_path}/scripts/seed.sql;",
+		unless    => '/usr/bin/mysql -pvagrant wiki -e "SELECT \'exists\' FROM page WHERE page_title = \'Hydriz\'" | /bin/grep -q exists',
+		user      => 'root',
+		logoutput => true,
+		require   => [Git::Clone['analytics/user-metrics'], Service['mysql']]
 	}
 
 	include apache
@@ -70,6 +88,6 @@ class user_metrics {
 
 	apache::site { $site_name:
 		content => template("user_metrics/virtual_host.erb"),
-		require =>  [Git::Clone['analytics/E3Analysis'], Apache::Mod['wsgi'], Apache::Mod['alias']],
+		require =>  [Git::Clone['analytics/user-metrics'], Apache::Mod['wsgi'], Apache::Mod['alias']],
 	}
 }
