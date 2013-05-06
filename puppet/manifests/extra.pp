38ee31843ae1517f03b1434d2976f69127e01729
Yuri Astrakhan <yurik@wikimedia.org>
2013-05-05 23:00:35 -0400

    Added a comment to run vagrant provision
    Change-Id: I5a24e68ffd3bc04b0902298ce6c1c6e375da2f94

--- puppet/manifests/extra.pp
+++ puppet/manifests/extra.pp
@@ -3,6 +3,8 @@
 # Uncomment lines below to enable Puppet modules that ship with Vagrant
 # but are not enabled by default.
 #
+# Once uncommented, run 'vagrant up' and 'vagrant provision'
+#
 
 # Selenium browser tests for MediaWiki
 # class { 'browsertests': }
