0ca2c3c3b4edbb42608b7b4298138a983b2a8c0d
Thibaut Horel <thibaut.horel@gmail.com>
2013-06-12 00:37:36 +0200

    Fix space/tab issue

--- puppet/manifests/roles.pp
+++ puppet/manifests/roles.pp
@@ -221,7 +221,7 @@ class role::proofreadpage {
 	include role::mediawiki
 
 	php::ini { 'proofreadpage':
-        settings => {
+		settings => {
 			'upload_max_filesize' => '50M',
 			'post_max_size' => '50M',
 		},
