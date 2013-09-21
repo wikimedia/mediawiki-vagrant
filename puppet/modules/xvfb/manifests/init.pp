b93a0d9326c78faf1f642b5abb16a1b32c29f7dd
Ori Livneh <ori@wikimedia.org>
2013-09-21 00:50:13 -0700

    Add Xvfb module
    Adds a module that installs the X virtual framebuffer package and
    configures an instance to run as a persistent Upstart service.
    May be used in conjunction with a graphical web browser.
    
    Change-Id: I899afbb0b7c35523d928c4d2ddcfaa3e3212911a

---
+++ puppet/modules/xvfb/manifests/init.pp
@@ -0,0 +1,53 @@
+# == Class: xvfb
+#
+# Xvfb or X virtual framebuffer is an X11 server that performs all
+# graphical operations in memory, not showing any screen output.
+# It's useful for automating graphical applications. This module
+# configures a persistent Xvfb daemon.
+#
+# === Parameters
+#
+# [*display*]
+#   X display number. Default: 99.
+#
+# [*resolution*]
+#   Virtual framebuffer resolution, expressed as WIDTHxHEIGHTxDEPTH.
+#   Default: '1024x768x24'.
+#
+# === Examples
+#
+#  class { 'xvfb':
+#    resolution => '800x600x16',
+#    display    => 100,
+#  }
+#
+class xvfb(
+    $display    = 99,
+    $resolution = '1024x768x24',
+) {
+    package { 'xvfb':
+        ensure => present,
+    }
+
+    group { 'xvfb':
+        ensure => present,
+    }
+
+    user { 'xvfb':
+        ensure => present,
+        gid    => 'xvfb',
+        shell  => '/bin/false',
+        home   => '/nonexistent',
+        system => true,
+    }
+
+    file { '/etc/init/xvfb.conf':
+        content => template('xvfb/xvfb.conf.erb'),
+        require => [ Package['xvfb'], User['xvfb'] ],
+    }
+
+    service { 'xvfb':
+        provider => 'upstart',
+        require  => File['/etc/init/xvfb.conf'],
+    }
+}
