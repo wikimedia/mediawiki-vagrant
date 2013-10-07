9539f82d0e0d7bcf0e40868df9e467eb7a9c577d
Kunal Mehta <legoktm@gmail.com>
2013-10-06 23:19:26 -0700

    Add role for MassMessage extension.
    Also includes LQT, Echo and MLEB (Translate) to test integration with those
    extensions.
    
    Change-Id: I6a716776aee089f4ea5e782799b9ae97733f5d6b

--- puppet/manifests/roles.pp
+++ puppet/manifests/roles.pp
@@ -599,3 +599,22 @@ class role::cirrussearch {
         before  => Mediawiki::Extension['CirrusSearch'],
     }
 }
+
+# == Class: role::massmessage
+# This role provisions the MassMessage extension, which allows users to
+# easily send a message to a list of pages via the job queue, and a set
+# of extensions which integrate with it: LiquidThreads, Echo, and MLEB.
+class role::massmessage {
+    include role::mediawiki
+    include role::echo
+    include role::mleb
+
+    @mediawiki::extension { 'MassMessage': }
+
+    @mediawiki::extension { 'LiquidThreads':
+        needs_update => true,
+        settings => {
+            wgLqtTalkPages => false,
+        },
+    }
+}
