95427550a19df597f8caef00c3cbdd9ffbde8265
Kunal Mehta <legoktm@gmail.com>
2013-10-06 22:51:40 -0700

    Add AntiSpam role
    Includes AntiSpoof, AbuseFilter and SpamBlacklist extensions
    
    Change-Id: Id6c2d79d620af53e25b76178098bbceb8d2d510a

--- puppet/manifests/roles.pp
+++ puppet/manifests/roles.pp
@@ -548,3 +548,33 @@ class role::mleb {
         require  => Mediawiki::Extension['Interwiki'],
     }
 }
+
+# == Class: role::antispam
+# Installs and sets up AntiSpoof, AbuseFilter, and the SpamBlacklist extensions
+class role::antispam {
+    include role::mediawiki
+
+    @mediawiki::extension { 'AntiSpoof':
+        needs_update => true,
+    }
+
+    @mediawiki::extension { 'AbuseFilter':
+        needs_update => true,
+        settings     => [
+            '$wgGroupPermissions["sysop"]["abusefilter-modify"] = true',
+            '$wgGroupPermissions["*"]["abusefilter-log-detail"] = true',
+            '$wgGroupPermissions["*"]["abusefilter-view"] = true',
+            '$wgGroupPermissions["*"]["abusefilter-log"] = true',
+            '$wgGroupPermissions["sysop"]["abusefilter-private"] = true',
+            '$wgGroupPermissions["sysop"]["abusefilter-modify-restricted"] = true',
+            '$wgGroupPermissions["sysop"]["abusefilter-revert"] = true',
+        ],
+        require => MediaWiki::Extension['AntiSpoof'],
+    }
+
+    @mediawiki::extension { 'SpamBlacklist':
+        settings => {
+            wgLogSpamBlacklistHits => true,
+        },
+    }
+}
