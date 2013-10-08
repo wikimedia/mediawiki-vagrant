028a8c5ad4fc2b23c4085e0c5aabb0204cd85bce
Ori Livneh <ori@wikimedia.org>
2013-10-08 01:15:45 -0700

    Drop spurious trailing '#' from comment block
    Follows Iaf9ccc259. Also expands the content of the comment-block a little.
    
    Change-Id: I0c9f776b5d78c05fc98b6b8faba247d56a57fcbf

--- puppet/manifests/roles.pp
+++ puppet/manifests/roles.pp
@@ -265,8 +265,9 @@ class role::uploadwizard {
     }
 }
 
-# == Class: role::codeeditor #
-# Configures CodeEditor, extension for using ACE for editing code
+# == Class: role::codeeditor
+# The CodeEditor extension embeds the ACE code editor in the WikiEditor
+# edit interface when source code content.
 class role::codeeditor {
     include role::mediawiki
     include role::wikieditor
