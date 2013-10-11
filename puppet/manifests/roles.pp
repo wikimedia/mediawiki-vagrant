31f7a9bd5de0780fc71e52bbfa514dbbfeab8b8c
Matthew Flaschen <mflaschen@wikimedia.org>
2013-10-11 01:40:36 -0400

    Add more docs on XDebug, and fix hostname
    Change-Id: I7a0bdd295521c153f5127974936f910177b14a03

--- puppet/manifests/roles.pp
+++ puppet/manifests/roles.pp
@@ -363,18 +363,20 @@ class role::proofreadpage {
 # This class enables support for remote debugging of PHP code using
 # Xdebug. Remote debugging allows you to interactively walk through your
 # code as executes. Remote debugging is most useful when used in
-# conjunction with a PHP IDE such as PhpStorm. The IDE is installed on
-# your machine, not the Vagrant VM.
+# conjunction with a PHP IDE such as PhpStorm or Emacs (with Geben).
+# The IDE is installed on your machine, not the Vagrant VM.
 #
 # -- To use, enable this role from shell:
 #    vagrant enable-role remote_debug
 # -- In your IDE, enable "Start Listening for PHP Debug Connections"
-# -- For firefox, install
+# -- For Firefox, install
 #    https://addons.mozilla.org/en-US/firefox/addon/the-easiest-xdebug
 #    and click "Enable Debug" icon in the Add-on bar
 # -- Set breakpoints
-# -- Navigate to localhost:8080/...
+# -- Navigate to 127.0.0.1:8080/...
 #
+# See https://www.mediawiki.org/wiki/MediaWiki-Vagrant/Advanced_usage#MediaWiki_debugging_using_Xdebug_and_an_IDE_in_your_host
+# for more information.
 class role::remote_debug {
     include php
 
