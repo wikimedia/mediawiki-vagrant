4d22a70402fd959f66d2e3d2ab6523b7192f0e12
Erik Bernhardson <ebernhardson@wikimedia.org>
2013-10-21 20:02:59 -0700

    Add role::profiler utilizing XHProf
    Adds a profiling role which installs xhprof from pecl and enables its web
    interface on /xhprof of the devwiki http server.
    
    One issue remaining in regards to utilizing disable-role.  The only part that
    gets uninstalled when removing the role is the apache configuration.  The php
    config installed by php::ini will still be in /etc/php5/conf.d/xhprof.ini and
    loaded. The module installed in /usr/lib/php/20090626/xhprof.so will also still
    be in place.  Additionally the assets and code files installed to /usr/share/php
    are left behind.  In general this isn't a big deal, but worth mentioning.
    
    Change-Id: I12b7041739ba1a4ba9b3c651c99c801123252e56

--- puppet/manifests/roles.pp
+++ puppet/manifests/roles.pp
@@ -404,6 +404,28 @@ class role::remote_debug {
     }
 }
 
+# == Class role::xhprof
+# This class enables support for function-level hierarchical profiling of PHP
+# using XHProf. The graphical interface for the profiler is available at
+# /xhprof on the same port as the wiki.
+#
+# You can add the following code to $IP/StartProfiler.php to use XHProf with
+# MediaWiki:
+#
+#   xhprof_enable( XHPROF_FLAGS_CPU | XHPROF_FLAGS_MEMORY );
+#   register_shutdown_function( function() {
+#       $profile = xhprof_disable();
+#       require_once 'xhprof_lib/utils/xhprof_runs.php';
+#       $runs = new XHProfRuns_Default();
+#       $runs->save_run( $profile, 'mw' );
+#   } );
+#
+class role::xhprof {
+    include role::mediawiki
+
+    include xhprof
+}
+
 # == Class: role::multimedia
 # This class configures MediaWiki for multimedia development.
 # It is meant to contain general configuration of shared use to other
