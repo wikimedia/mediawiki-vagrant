0cbae9b59c9eadc4ee1b61b94ec6068fabe11b9a
Yuvi Panda <yuvipanda@gmail.com>
2013-11-07 16:34:46 -0800

    Add APISandbox Role
    Simply enables the APISandbox extension, does nothing else.
    
    Reading the huge wall of text generated by /w/api.php is for chumps.
    
    Change-Id: Ica747215f1c35908548735a303c193b397ab634e

--- puppet/manifests/roles.pp
+++ puppet/manifests/roles.pp
@@ -648,3 +648,11 @@ class role::massmessage {
         },
     }
 }
+
+# == Class: role::apisandbox
+# This role simply sets up the ApiSandbox extension
+class role::apisandbox {
+    include role::mediawiki
+
+    @mediawiki::extension { 'ApiSandbox': }
+}
