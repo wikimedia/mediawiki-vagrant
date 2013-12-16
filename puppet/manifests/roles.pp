c8ca5e34e63608618a1c312ba20838f0fa38f77e
Ori Livneh <ori@wikimedia.org>
2013-12-16 15:17:26 -0800

    Add NavigationTiming role
    Adds a role for extension:NavigationTiming.
    
    Change-Id: I8380abb1fecb47591319ff7e7770f699b58a1ed4

--- puppet/manifests/roles.pp
+++ puppet/manifests/roles.pp
@@ -681,6 +681,16 @@ class role::apisandbox {
     @mediawiki::extension { 'ApiSandbox': }
 }
 
+# == Class: role::navigationtiming
+# Sets up NavigationTiming, an extension for logging client-side latency
+# measurements via EventLogging.
+class role::navigationtiming {
+    include role::mediawiki
+    include role::eventlogging
+
+    @mediawiki::extension { 'NavigationTiming': }
+}
+
 # == Class: role::timedmediahandler
 # This role provisions the TimedMediaHandler extension,
 # which displays audio and video files and their captions.
