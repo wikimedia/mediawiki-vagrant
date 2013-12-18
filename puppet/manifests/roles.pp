f0284ef88cfd38d90d6775672d34b57a52924c1f
csteipp <csteipp@wikimedia.org>
2013-12-18 15:45:11 -0800

    Add OAuth as a role
    Install Extension:OAuth with needed permissions
    
    Change-Id: I95f4d5e861a3e9c1e098d91b3c09b1aa5dc56b67

--- puppet/manifests/roles.pp
+++ puppet/manifests/roles.pp
@@ -149,6 +149,25 @@ class role::eventlogging {
     }
 }
 
+# == Class: role::oauth
+# This role sets up the OAuth extension for MediaWiki. Other OAuth
+# enabled applications can then edit this instance of MediaWiki on
+# its users' behalf.
+class role::oauth {
+    include role::mediawiki
+
+    @mediawiki::extension { 'OAuth':
+        needs_update => true,
+        settings => [
+            '$wgMWOAuthSecureTokenTransfer = false',
+            '$wgGroupPermissions["sysop"]["mwoauthmanageconsumer"] = true',
+            '$wgGroupPermissions["sysop"]["mwoauthviewprivate"] = true',
+            '$wgGroupPermissions["user"]["mwoauthproposeconsumer"] = true',
+            '$wgGroupPermissions["user"]["mwoauthupdateownconsumer"] = true',
+        ]
+    }
+}
+
 # == Class: role::mobilefrontend
 # Configures MobileFrontend, the MediaWiki extension which powers
 # Wikimedia mobile sites.
