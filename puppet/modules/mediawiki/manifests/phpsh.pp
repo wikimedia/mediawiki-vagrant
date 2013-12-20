7914ea177011a722282dcb35afab933143f296a2
Matthew Flaschen <mflaschen@wikimedia.org>
2013-12-20 17:10:17 -0500

    Update to phpsh 1.3.3 (latest of Ori's pip versions)
    This includes important memory limit-related fixes, e.g. making phpsh
    understand -1 is unlimited memory, and should not be changed.
    
    Change-Id: I2918be2da5fc55a29a6f2ad3f15ecebbfc3e04f6

--- puppet/modules/mediawiki/manifests/phpsh.pp
+++ puppet/modules/mediawiki/manifests/phpsh.pp
@@ -8,7 +8,7 @@ class mediawiki::phpsh {
     include php
 
     package { 'phpsh':
-        ensure   => '1.3.1',
+        ensure   => '1.3.3',
         provider => pip,
         require  => Package['php5'],
     }
