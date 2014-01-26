fe81da6bcfb74d0f9459c4025e26efb0d1e1db82
Ori Livneh <ori@wikimedia.org>
2014-01-26 15:14:21 -0800

    Add a role for the Gadgets extension
    By request! :)
    
    Bug: 60446
    Change-Id: Idcba626e636a4af6b6a299f1634b1260c79fad0d

--- puppet/manifests/roles.pp
+++ puppet/manifests/roles.pp
@@ -892,3 +892,11 @@ class role::hive {
         require => Class['::cdh4::hive'],
     }
 }
+
+
+# == Class role::gadgets
+# The Gadgets extension provides a way for users to pick JavaScript
+# or CSS based "gadgets" that other wiki users provide.
+class role::gadgets {
+    mediawiki::extension { 'Gadgets': }
+}
