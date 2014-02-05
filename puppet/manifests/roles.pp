c270c067cea08d2c0c51121a4c7dbb6facfefea6
Yuvi Panda <yuvipanda@gmail.com>
2014-02-06 01:37:04 +0530

    Add role for WikiLove
    SHOW ME LOVE!
    
    Change-Id: Ied8c6197ed01d96bc7b6e75c8a5860830e61ae63

--- puppet/manifests/roles.pp
+++ puppet/manifests/roles.pp
@@ -1018,3 +1018,15 @@ class role::geodata {
         priority     => $LOAD_LATER,  # Should come after either CirrusSearch or Solarium
     }
 }
+
+# == Class role::wikilove
+# The WikiLove extension lets people send love to other wiki users
+# in the form of the Internet's most preferred currency, kittens
+class role::wikilove {
+    mediawiki::extension { 'WikiLove':
+        needs_update           => true,
+        settings               => {
+            'wgWikiLoveGlobal' => 'true'
+        }
+    }
+}
