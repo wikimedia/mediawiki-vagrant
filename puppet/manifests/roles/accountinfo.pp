df4ee618cd9ae133fd6e8faf8186bc34bf2951f8
Kunal Mehta <legoktm@gmail.com>
2014-02-17 22:50:36 -0800

    Add role for AccountInfo extension
    Change-Id: Ifde6dc2970edc922e0825443d21251456b87ab32

---
+++ puppet/manifests/roles/accountinfo.pp
@@ -0,0 +1,22 @@
+# == Class: role::accountinfo
+# The AccountInfo extension allows users to look at
+# private information that is stored about them.
+# It also includes the CheckUser extension, which
+# AccountInfo integrates with.
+class role::accountinfo {
+    include role::mediawiki
+
+    mediawiki::extension { 'CheckUser':
+        needs_update => true,
+    }
+
+    mediawiki::extension { 'AccountInfo':
+        settings     => {
+            wgPutIPinRC => true,
+        },
+        require => [
+            Mediawiki::Extension['CheckUser'],
+        ],
+
+    }
+}
