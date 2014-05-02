6d6fc8d950802f46562ff602cc07983c1b5f21e3
Juliusz Gonera <jgonera@wikimedia.org>
2014-05-01 19:05:06 -0700

    Add betafeatures as a dependency for popups role
    Popups (Hovercards) is a beta feature, it can't be activated without
    BetaFeatures.
    
    Also, remove mediawiki role as a dependency (redundant).
    
    Change-Id: Ice465d551fcebe2bfeee33e9c548a0cdd5ff13b2

--- puppet/manifests/roles/popups.pp
+++ puppet/manifests/roles/popups.pp
@@ -2,10 +2,10 @@
 # The Popups extension shows a popup when people hover over article
 # links.
 class role::popups {
-    include role::mediawiki
     include role::eventlogging
     include role::pageimages
     include role::textextracts
+    include role::betafeatures
 
     mediawiki::extension { 'Popups': }
 }
