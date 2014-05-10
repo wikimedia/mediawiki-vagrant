9e64a2209be30bfe07e41834bdcb65375b454ffc
Andrew Bogott <abogott@wikimedia.org>
2014-05-10 14:43:32 +0200

    Add the mediawiki::import_dump resource, add a role for labs that uses it.
    Change-Id: I6dfac2060ab96d41d26dc765f6267ec2cd684084

---
+++ puppet/manifests/roles/labs_initial_content.pp
@@ -0,0 +1,32 @@
+# == Class: role::labs_initial_wiki
+#
+#  For labs:  loads a ready-made logo and privacy policy into the
+#  initial wiki.
+#
+class role::labs_initial_content {
+    mediawiki::import_dump { 'labs_privacy':
+        require            => Class['::role::mediawiki'],
+        xml_dump           => '/vagrant/puppet/modules/labs/files/labs_privacy_policy.xml',
+        dump_sentinel_page => 'Testwiki:Privacy_policy',
+    }
+
+    file { '/var/www/labs_mediawiki_logo.png':
+        require  => Class['::role::mediawiki'],
+        ensure   => present,
+        source   => '/vagrant/puppet/modules/labs/files/labs_mediawiki_logo.png'
+    }
+
+    file { '/var/www/robots.txt':
+        require  => Class['::role::mediawiki'],
+        ensure   => present,
+        source   => '/vagrant/puppet/modules/labs/files/robots.txt'
+    }
+
+    mediawiki::settings { 'labs-vagrant logo':
+        values => {
+            wgLogo          => '/labs_mediawiki_logo.png',
+            wgMetaNamespace => 'Testwiki',
+        },
+    }
+
+}
---
+++ puppet/modules/mediawiki/manifests/import_dump.pp
@@ -0,0 +1,24 @@
+# Imports an xml dump into the wiki.
+#
+#  $xml_dump should be the /fully/qualified/path to a dump
+#
+#  $dump_sentinel_page should specify a page unique to the dump.  As long as
+#   $dump_sentinel_page is not present in the wiki we will keep trying the import.
+#
+# We try to do this only on the first run and not clobber existing imports.
+#
+define mediawiki::import_dump(
+    $xml_dump,
+    $dump_sentinel_page,
+) {
+    include mediawiki
+
+    exec { 'import_dump':
+        require   => Class['mediawiki'],
+        cwd       => "${mediawiki::dir}/maintenance",
+        command   => "/usr/bin/php importDump.php ${xml_dump}",
+        unless    => "/usr/bin/php pageExists.php ${dump_sentinel_page}",
+        user      => 'www-data',
+        logoutput => on_failure,
+    }
+}
