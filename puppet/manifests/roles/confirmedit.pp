235f50ac5b6093122bc7c6821c11377a0d6273a3
Juliusz Gonera <jgonera@wikimedia.org>
2014-05-11 00:41:39 +0200

    Use FancyCaptcha in ConfirmEdit
    That's what we currently use in production.
    
    Change-Id: I8544c32871da73d6199a75a0ba70a0e3a6543ebe

--- puppet/manifests/roles/confirmedit.pp
+++ puppet/manifests/roles/confirmedit.pp
@@ -5,8 +5,24 @@
 # to guess passwords.
 class role::confirmedit {
     include role::mediawiki
+    include python::pil
 
     mediawiki::extension { 'ConfirmEdit':
-        settings => { wgCaptchaClass => 'SimpleCaptcha' },
+		before => Exec['generate FancyCaptcha images']
+	}
+
+    mediawiki::settings { 'ConfirmEdit FancyCaptcha':
+        values => {
+            wgCaptchaClass => 'FancyCaptcha',
+            wgCaptchaDirectory => '$IP/images/temp/captcha',
+            wgCaptchaDirectoryLevels => 0,
+            wgCaptchaSecret => 'FOO',
+        },
+        header => 'require_once "$IP/extensions/ConfirmEdit/FancyCaptcha.php";',
+    }
+
+    exec { 'generate FancyCaptcha images':
+        command => 'echo "hello\nworld\n" > /tmp/words; mkdir -p ../../images/temp/captcha; python captcha.py --font=/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf --wordlist=/tmp/words --key=FOO --output=../../images/temp/captcha --count=2',
+        cwd     => '/vagrant/mediawiki/extensions/ConfirmEdit',
     }
 }
--- puppet/modules/python/manifests/pil.pp
+++ puppet/modules/python/manifests/pil.pp
@@ -8,6 +8,8 @@ class python::pil {
     include ::python::dev
 
     package { 'zlib1g-dev': }
+    # needed for ConfirmEdit's FancyCaptcha
+    package { 'libfreetype6-dev': }
 
     # Workaround for 'pip install pil' failing to find libz.so and thus
     # installing without zlib support. See <http://goo.gl/eWJc24>.
@@ -17,12 +19,20 @@ class python::pil {
         require => Package['zlib1g-dev'],
     }
 
+    # Workaround for 'pip install pil' failing to find libfreetype.so and
+    # thus installing without freetype support. See <http://goo.gl/eWJc24>.
+    file { '/usr/lib/libfreetype.so':
+        ensure  => link,
+        target  => "/usr/lib/${::hardwaremodel}-linux-gnu/libfreetype.so",
+        require => Package['libfreetype6-dev'],
+    }
+
     package { 'PIL':
         ensure   => '1.1.7',
         provider => 'pip',
         require  => [
-            Package['python-dev', 'zlib1g-dev'],
-            File['/usr/lib/libz.so'],
+            Package['python-dev', 'zlib1g-dev', 'libfreetype6-dev'],
+            File['/usr/lib/libz.so', '/usr/lib/libfreetype.so'],
         ],
     }
 }
