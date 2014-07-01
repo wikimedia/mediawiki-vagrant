aaef5679959e79fd18cf2514809a278a6006f392
Yuri Astrakhan <yurik@wikimedia.org>
2014-07-01 00:58:27 -0400

    Added jsonconfig & zero roles
    Change-Id: I160a65582f8187c5098662e2a0300841b2c925b9

---
+++ puppet/manifests/roles/jsonconfig.pp
@@ -0,0 +1,11 @@
+# == Class: role::jsonconfig
+# The extension that allows other extensions to store JSON text in wiki pages
+# See http://www.mediawiki.org/wiki/Extension:JsonConfig
+class role::jsonconfig {
+    include role::mediawiki
+    include role::codeeditor   # optional - looks better for editing
+
+    mediawiki::extension { 'JsonConfig':
+        priority => $::LOAD_EARLY, # ensure that extensions that use JsonConfig will load later
+    }
+}
---
+++ puppet/manifests/roles/zero.pp
@@ -0,0 +1,25 @@
+# == Class: role::zero
+# Configures Zero for local use
+class role::zero {
+    include role::mediawiki
+    include role::mobilefrontend
+    include role::jsonconfig
+    include role::thumb_on_404
+
+    mediawiki::extension { 'ZeroBanner':
+        priority => $::LOAD_LATER, # Must be after JsonConfig & MobileFrontEnd
+        settings => [
+            '$wgMobileUrlTemplate = "%h0.m.%h1.%h2"',
+            '$wgZeroEnableTesting = true',
+            '$wgGroupPermissions["sysop"]["zero-edit"] = true',
+            '$wgGroupPermissions["sysop"]["zero-script"] = true',
+        ],
+    }
+
+    mediawiki::extension { 'ZeroPortal':
+        priority => $::LOAD_LAST, # Must be after ZeroBanner
+        settings => [
+            '$wgJsonConfigs["JsonZeroConfig"]["store"] = true',
+        ],
+    }
+}
