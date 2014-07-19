555d53595c0c1959026a7879af925b949d7a7550
Bryan Davis <bd808@bd808.com>
2014-07-19 17:11:19 -0600

    Alphabetize packages.pp
    Change-Id: I277e71e7975864504186528956670ea0e1fdb7b7

--- puppet/manifests/packages.pp
+++ puppet/manifests/packages.pp
@@ -11,82 +11,48 @@
 # *Note*:: each class in this file must correspond to a package of the
 #   same name. If you need anything fancy, create a module instead.
 #
-
-class packages::drush {
-    package { 'drush': }
-}
-
-class packages::imagemagick {
-    package { 'imagemagick': }
-}
-
-class packages::ghostscript {
-    package { 'ghostscript': }
-}
-
-class packages::poppler_utils {
-    package { 'poppler-utils': }
-}
-
-class packages::rsyslog {
-    package { 'rsyslog': }
-}
-
-class packages::php_luasandbox {
-    package { 'php-luasandbox': }
-}
-
-class packages::php5_tidy {
-    package { 'php5-tidy': }
-}
+# *Note*:: please add new classes in alphabetical order to make finding
+#   things easier.
+#
 
 class packages::djvulibre_bin {
     package { 'djvulibre-bin': }
 }
 
-class packages::netpbm {
-    package { 'netpbm': }
+class packages::drush {
+    package { 'drush': }
 }
 
-class packages::mediawiki_math {
-    package { 'mediawiki-math': }
-}
-
-class packages::ocaml_native_compilers {
-    package { 'ocaml-native-compilers': }
-}
-
-class packages::libav_tools {
-    package { 'libav-tools': }
+class packages::exiv2 {
+    package { 'exiv2': }
 }
 
 class packages::ffmpeg2theora {
     package { 'ffmpeg2theora': }
 }
 
+class packages::fonts_dejavu {
+    package { 'fonts-dejavu': }
+}
+
+class packages::fss {
+    package { 'php5-fss': }
+}
+
+class packages::ghostscript {
+    package { 'ghostscript': }
+}
+
+class packages::imagemagick {
+    package { 'imagemagick': }
+}
+
 class packages::java {
     package { 'openjdk-7-jdk': }
 }
 
-class packages::wikitools {
-    package { 'wikitools':
-        ensure   => '1.1',
-        provider => 'pip',
-    }
-}
-
-class packages::poster {
-    package { 'poster':
-        ensure   => '0.8.0',
-        provider => 'pip',
-    }
-}
-
-class packages::nose {
-    package { 'nose':
-        ensure   => '1.3.0',
-        provider => 'pip',
-    }
+class packages::jq {
+    package { 'jq': }
 }
 
 class packages::jsduck {
@@ -96,38 +62,75 @@ class packages::jsduck {
     }
 }
 
-class packages::pil {
-    package { 'python-imaging': }
-}
-
-class packages::wikidiff2 {
-    package { 'php-wikidiff2': }
-}
-
-class packages::jq {
-    package { 'jq': }
-}
-
-class packages::fss {
-    package { 'php5-fss': }
-}
-
-class packages::unzip {
-    package { 'unzip': }
-}
-
-class packages::exiv2 {
-    package { 'exiv2': }
-}
-
-class packages::fonts_dejavu {
-    package { 'fonts-dejavu': }
+class packages::libav_tools {
+    package { 'libav-tools': }
 }
 
 class packages::libtiff_tools {
     package { 'libtiff-tools': }
 }
 
+class packages::mediawiki_math {
+    package { 'mediawiki-math': }
+}
+
+class packages::netpbm {
+    package { 'netpbm': }
+}
+
+class packages::nose {
+    package { 'nose':
+        ensure   => '1.3.0',
+        provider => 'pip',
+    }
+}
+
+class packages::ocaml_native_compilers {
+    package { 'ocaml-native-compilers': }
+}
+
+class packages::php5_tidy {
+    package { 'php5-tidy': }
+}
+
+class packages::php_luasandbox {
+    package { 'php-luasandbox': }
+}
+
+class packages::pil {
+    package { 'python-imaging': }
+}
+
+class packages::poppler_utils {
+    package { 'poppler-utils': }
+}
+
+class packages::poster {
+    package { 'poster':
+        ensure   => '0.8.0',
+        provider => 'pip',
+    }
+}
+
+class packages::rsyslog {
+    package { 'rsyslog': }
+}
+
+class packages::unzip {
+    package { 'unzip': }
+}
+
 class packages::wbritish_small {
     package { 'wbritish-small': }
+}
+
+class packages::wikidiff2 {
+    package { 'php-wikidiff2': }
+}
+
+class packages::wikitools {
+    package { 'wikitools':
+        ensure   => '1.1',
+        provider => 'pip',
+    }
 }
