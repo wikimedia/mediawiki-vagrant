209da0d29edc9986f68c56bd5ea51122c31a8446
YuviPanda <yuvipanda@gmail.com>
2014-07-23 02:18:12 +0530

    Added MobileApp role
    Change-Id: I82739663fe6d9eb6c1aa5c53be16a0614ef9b970

---
+++ puppet/manifests/roles/mobileapp.pp
@@ -0,0 +1,10 @@
+# == Class: role::mobileapp
+# Configures MobileApp, which produces CSS files and hooks
+# for the Wikimedia Android & iOS Mobile apps
+class role::mobileapp {
+    include role::mobilefrontend
+
+    mediawiki::extension { 'MobileApp':
+        require => Mediawiki::Extension['MobileFrontend'],
+    }
+}
