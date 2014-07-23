69cc89b23f5829f35319c881a53cff9e933a3995
Kunal Mehta <legoktm@gmail.com>
2014-07-22 22:14:01 -0700

    Remove mleb dependency for MassMessage role
    It was added for an experimental feature which hasn't been merged
    into the extension yet, and probably won't be for a while.
    
    Change-Id: I0c43493b399320a2f735e9cc29f02455f2b694db

--- puppet/manifests/roles/massmessage.pp
+++ puppet/manifests/roles/massmessage.pp
@@ -1,11 +1,10 @@
 # == Class: role::massmessage
 # This role provisions the MassMessage extension, which allows users to
 # easily send a message to a list of pages via the job queue, and a set
-# of extensions which integrate with it: LiquidThreads, Echo, and MLEB.
+# of extensions which integrate with it: LiquidThreads and Echo.
 class role::massmessage {
     include role::mediawiki
     include role::echo
-    include role::mleb
 
     mediawiki::extension { 'MassMessage': }
 
