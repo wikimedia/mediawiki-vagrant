ad1771866b706aec59f230e607a9a98db33f9c63
Bryan Davis <bd808@bd808.com>
2014-07-24 22:13:36 -0600

    Add php5 vhost for main wiki
    Provision a vhost running the primary wiki using php5's mod_php
    interpreter. This can be useful when debugging php5 vs hhvm behavior
    differences. The vhost is available at http://php5.local.wmftest.net:8080/
    
    Bug: 67517
    Change-Id: I43ab9de1bdf4e1a1118752b73a3161c586b12854

--- puppet/modules/mediawiki/manifests/apache.pp
+++ puppet/modules/mediawiki/manifests/apache.pp
@@ -29,6 +29,12 @@ class mediawiki::apache(
         require => Class['::apache::mod::alias', '::apache::mod::rewrite', '::apache::mod::proxy_fcgi'],
     }
 
+    apache::site { "php5-${mediawiki::wiki_name}":
+        ensure  => present,
+        content => template('mediawiki/mediawiki-php5-site.erb'),
+        require => Class['::apache::mod::alias', '::apache::mod::rewrite'],
+    }
+
     file { "${docroot}/favicon.ico":
         ensure  => file,
         require => Package['apache2'],
