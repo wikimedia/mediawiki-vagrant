e54d4dec6a1309dd206d523c1159f41907ab1e74
Bryan Davis <bd808@bd808.com>
2014-08-02 23:12:59 -0600

    Add git::install resource and fix git::clone
    Introduces git::install resource which can be used to clone a git
    repository and checkout a given commit (or other tree-ish reference). By
    default this checkout will be maintained in the specified state.
    
    Also cleans up some features added to git::clone in Iad0c782:
    * ensure => absent support is dropped
    * Exec["git_pull_${title}"] no longer suppresses output on
      error
    * Unless guard for Exec["git_pull_${title}"] updated to work with
      branch tracking
    * Git pull for ensure=>latest limited to running once an hour via
      schedule
    
    Change-Id: I6215abca89945009008f8acae15dc4810664c6c5

--- puppet/modules/git/manifests/clone.pp
+++ puppet/modules/git/manifests/clone.pp
@@ -25,8 +25,8 @@
 #   Group that should own the checked out repostory. Default 'vagrant'.
 #
 # [*ensure*]
-#   What state the clone should be in. Valid values are `present`, `absent`
-#   and `latest`. Default 'present'.
+#   What state the clone should be in. Valid values are `present` and
+#   `latest`. Default 'present'.
 #
 # [*depth*]
 #   If specified, creates a shallow clone with history truncated to the
@@ -54,10 +54,10 @@ define git::clone(
     $depth              = undef,
     $recurse_submodules = true,
 ) {
-    include ::git
+    require ::git
 
-    if !($ensure in ['absent', 'present', 'latest']) {
-        fail('ensure parameter must be absent, present, or latest.')
+    if !($ensure in ['present', 'latest']) {
+        fail('ensure parameter must be present or latest.')
     }
 
     $repository = $remote ? {
@@ -65,59 +65,48 @@ define git::clone(
         default => $remote,
     }
 
-    case $ensure {
-        'absent': {
-            file { $directory:
-                ensure  => 'absent',
-                recurse => true,
-                force   => true,
-            }
+    $arg_branch = $branch ? {
+        undef   => '',
+        default => "--branch '${branch}'"
+    }
+    $arg_recurse = $recurse_submodules ? {
+        true    => '--recurse-submodules',
+        default => '',
+    }
+    $arg_depth = $depth ? {
+        undef => '',
+        default => "--depth=${depth}",
+    }
+
+    exec { "git_clone_${title}":
+        command     => "git clone ${arg_recurse} ${arg_depth} ${arg_branch} ${repository} ${directory}",
+        cwd         => '/',
+        creates     => "${directory}/.git",
+        user        => $owner,
+        group       => $group,
+        require     => Package['git'],
+        timeout     => 0,
+    }
+
+    if $ensure == 'latest' {
+        exec { "git_pull_${title}":
+            command  => "git pull ${arg_recurse} ${arg_depth}",
+            unless   => 'git fetch && git diff --quiet @{upstream}',
+            cwd      => $directory,
+            user     => $owner,
+            group    => $group,
+            schedule => 'hourly',
+            require  => Exec["git_clone_${title}"],
         }
 
-        default: {
-            $arg_branch = $branch ? {
-                undef   => '',
-                default => " --branch '${branch}'"
-            }
-            $arg_recurse = $recurse_submodules ? {
-                true    => ' --recurse-submodules',
-                default => '',
-            }
-            $arg_depth = $depth ? {
-                undef => '',
-                default => " --depth=${depth}",
-            }
-
-            exec { "git_clone_${title}":
-                command     => "git clone${arg_recurse}${arg_depth}${arg_branch} ${repository} ${directory}",
-                cwd         => '/',
-                creates     => "${directory}/.git",
+        if $recurse_submodules {
+            exec { "git_submodule_update_${title}":
+                command     => 'git submodule update --init --recursive',
+                cwd         => $directory,
                 user        => $owner,
                 group       => $group,
-                require     => Package['git'],
-                timeout     => 0,
-            }
-
-            if $ensure == 'latest' {
-                exec { "git_pull_${title}":
-                    command => "git pull${arg_recurse}${arg_depth} --quiet",
-                    unless  => 'git fetch && git diff --quiet remotes/origin/HEAD',
-                    cwd     => $directory,
-                    user    => $owner,
-                    group   => $group,
-                    require => Exec["git_clone_${title}"],
-                }
-
-                if $recurse_submodules {
-                    exec { "git_submodule_update_${title}":
-                        command     => 'git submodule update --init --recursive',
-                        cwd         => $directory,
-                        user        => $owner,
-                        group       => $group,
-                        refreshonly => true,
-                        subscribe   => Exec["git_pull_${title}"],
-                    }
-                }
+                refreshonly => true,
+                subscribe   => Exec["git_pull_${title}"],
             }
         }
     }
---
+++ puppet/modules/git/manifests/install.pp
@@ -0,0 +1,73 @@
+# == Define: git::install
+#
+# Creates a git clone of a specified wikimedia project into a directory,
+# and checks out the specfied commit.
+#
+# === Parameters
+#
+# [*directory*]
+#   Path to clone the repository into.
+#
+# [*commit*]
+#   The commit to checkout in the repository. This can specify a commit by
+#   hash (short or full), a tag or a tree.
+#
+# [*remote*]
+#   Remote URL for the repository. If unspecified, the resource title
+#   will be interpolated into $git::urlformat.
+#
+# [*owner*]
+#   User that should own the checked out repository. Git commands will run as
+#   this user so the user must have the ability to create the target
+#   directory. Default 'root'.
+#
+# [*group*]
+#   Group that should own the checked out repostory. Default 'root'.
+#
+# [*preserve_commit*]
+#   Keep the working copy at the specified commit. Default true.
+#
+# === Example usage
+#
+#   git::install { 'project/name/on/gerrit':
+#       directory     => '/some/path/here',
+#       commit        => 'tags/my-preferred-tag',
+#       post_checkout => 'puppet://files/some/script/that/should/run/post/checkout'
+#   }
+#
+define git::install(
+    $directory,
+    $commit,
+    $remote          = undef,
+    $owner           = 'root',
+    $group           = 'root',
+    $preserve_commit = true,
+) {
+    require ::git
+
+    git::clone { $title:
+        directory => $directory,
+        owner     => $owner,
+        group     => $group,
+        remote    => $remote,
+        notify    => Exec["git_install_checkout_${title}"],
+    }
+
+    exec { "git_install_checkout_${title}":
+        command     => "git remote update && git checkout ${commit}",
+        cwd         => $directory,
+        user        => $owner,
+        refreshonly => true
+    }
+
+    if $preserve_commit {
+        exec { "git_install_reset_${title}":
+            command   => 'git clean -d --force & git checkout -- .',
+            cwd       => $directory,
+            user      => $owner,
+            unless    => "git diff HEAD..${commit} --exit-code",
+            notify    => Exec["git_install_checkout_${title}"],
+            require   => Git::Clone[$title],
+        }
+    }
+}
