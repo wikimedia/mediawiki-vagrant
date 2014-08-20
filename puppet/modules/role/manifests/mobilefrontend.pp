ab3db8d9916aedc0cc7a384c4d6f773f3ac89c0f
Bryan Davis <bd808@bd808.com>
2014-08-20 07:57:35 -0600

    Provision mobile wiki with role::mobilefrontend
    Add an always mobile skinned wiki when role::mobilefrontend is enabled.
    The existing behavior of user-agent based theme switching is preserved
    for other wikis including the default wiki. Switching on the new
    "mobile.wiki.local.wmftest.net" is managed by setting $wgMFMobileHeader
    to "Host" which will always be present in the request.
    
    Change-Id: Icf55f679d1592aa68e04d294510f7d0f612791bc

--- puppet/modules/role/manifests/mobilefrontend.pp
+++ puppet/modules/role/manifests/mobilefrontend.pp
@@ -1,7 +1,13 @@
 # == Class: role::mobilefrontend
 # Configures MobileFrontend, the MediaWiki extension which powers
 # Wikimedia mobile sites.
+#
+# All wikis will have the MobileFrontend skin enabled based on user-agent
+# switching. Additionally, a "mobile.wiki.local.wmftest.net" wiki is created
+# which will always use the mobile skin reguardless of user-agent.
+#
 class role::mobilefrontend {
+    require ::role::mediawiki
     include ::role::eventlogging
     include ::role::mantle
     include ::role::pageimages
@@ -15,5 +21,16 @@ class role::mobilefrontend {
             wgMFNearby               => true,
         },
         browser_tests => true,
+    }
+
+    mediawiki::wiki{ 'mobile': }
+
+    mediawiki::settings { 'AlwaysMobileSkin':
+        wiki   => 'mobile',
+        values => {
+            wgMFAutodetectMobileView => false,
+            wgMFMobileHeader         => 'Host',
+            wgMobileUrlTemplate      => "mobile${::mediawiki::multiwiki::base_domain}",
+        }
     }
 }
