9573bad1b567b4ebcaf8e126487fe836eae40dd9
Max Semenik <maxsem.wiki@gmail.com>
2014-08-27 23:02:16 +0400

    Add a role that converts all PHP warnings/notices into errors
    Change-Id: Ibea47a3522e6002b13ece629620056ab9facdcca

---
+++ puppet/modules/role/manifests/warnings_as_errors.pp
@@ -0,0 +1,7 @@
+# == Class: role::warnings_as_errors
+# Turns all PHP warnings and notices into errors, useful for testing
+class role::warnings_as_errors {
+    mediawiki::settings { 'warnings_as_errors':
+        values => template( 'role/warnings_as_errors.php.erb' ),
+    }
+}
