34addef2cb62cf9c3d4b75b5927c591783989002
Kunal Mehta <legoktm@gmail.com>
2014-09-05 03:34:25 -0700

    Add globaluserpage role
    Provisions a wiki named "gup" to serve as the central wiki.
    
    Change-Id: Id4d111c70aaaa940717c3e24d1882dc88796c48d

---
+++ puppet/modules/role/manifests/globaluserpage.pp
@@ -0,0 +1,19 @@
+# == Class: role::globaluserpage
+# The GlobalUserPage extension allows for a
+# user to create a user page on a central wiki
+# and have it displayed on all wikis where a local
+# page does not exist.
+#
+class role::globaluserpage {
+    require ::role::mediawiki
+
+    mediawiki::extension { 'GlobalUserPage':
+        settings => {
+            wgGlobalUserPageDBname      => 'gupwiki',
+            wgGlobalUserPageAPIUrl      => "http://gup${::mediawiki::multiwiki::base_domain}${::port_fragment}/w/api.php",
+        }
+    }
+
+
+    mediawiki::wiki { 'gup': }
+}
