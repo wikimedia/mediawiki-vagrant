22fdb8de12bc9253deb1e1b46041a1da4381ab7e
dan-nl <d_entous@yahoo.com>
2014-09-07 11:51:41 +0200

    WikimediaMessages Role
    Provisions the WikimediaMessages[1] extension, which adds Wikimedia specific
    messages and grammar, as well as Wikimedia specific overrides for license
    related messages.
    
    [1]https://www.mediawiki.org/wiki/Extension:WikimediaMessages
    
    Change-Id: I29ff6a8dc31e532765d08aa54ccfa03d412d7ff7

---
+++ puppet/modules/role/manifests/wikimediamessages.pp
@@ -0,0 +1,11 @@
+# == Class: role::wikimediamessages
+# Provisions the WikimediaMessages[1] extension, which adds Wikimedia specific
+# messages and grammar, as well as Wikimedia specific overrides for license
+# related messages.
+#
+# [1]https://www.mediawiki.org/wiki/Extension:WikimediaMessages
+class role::wikimediamessages {
+  mediawiki::extension { 'WikimediaMessages':
+    settings => template('role/wikimediamessages-config.php.erb')
+  }
+}
