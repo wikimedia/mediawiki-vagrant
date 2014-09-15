071af2c132fa5a9e4f2dfe1b53daab6d2ffcb9d6
Dan Duvall <dduvall@wikimedia.org>
2014-09-15 16:47:36 -0700

    Split HHVM php.ini into common and fcgi hiera settings
    Refactored hhvm class to write separate common and fcgi-specific php.ini
    files based on settings pulled in from hiera. This approach is similar
    to that of the ops version but use of hiera over puppet hashes should
    allow for greater flexibility in configuration and a more generalized
    manifest.
    
    The max_execution_time setting was moved to the FCGI settings to avoid
    execution timeouts in CLI scripts (e.g. composer).
    
    Change-Id: Ifbfc38200808053d1b9700b9260d50110a5eccfa
    Bug: 70843

--- puppet/modules/hhvm/manifests/init.pp
+++ puppet/modules/hhvm/manifests/init.pp
@@ -1,10 +1,43 @@
 # == Class: hhvm
 #
-# Configures the HipHop Virtual Machine for PHP, a fast PHP interpreter
-# that is mostly compatible with the Zend interpreter. This module is a
-# work-in-progress.
+# This module provisions HHVM -- an open-source, high-performance
+# virtual machine for PHP.
+#
+# The layout of configuration files in /etc/hhvm is as follows:
+#
+#   /etc/hhvm
+#   │
+#   ├── config.hdf        ┐
+#   │                     ├ Settings for CLI mode
+#   ├── php.ini           ┘
+#   │
+#   └── fcgi
+#       │
+#       ├── config.hdf    ┐
+#       │                 ├ Settings for FastCGI mode
+#       └── php.ini       ┘
+#
+# The CLI configs are located in the paths HHVM automatically loads by
+# default. This makes it easy to invoke HHVM from the command line,
+# because no special arguments are required.
+#
+# HHVM is in the process of standardizing on the INI file format for
+# configuration files. At the moment (Aug 2014) there are still some
+# options that can only be set using the deprecated HDF syntax. This
+# is why we have two configuration files for each SAPI.
+#
+# The exact purpose of certain options is a little mysterious. The
+# documentation is getting better, but expect to have to dig around in
+# the source code.
 #
 # == Parameters:
+#
+# [*common_settings*]
+#   A hash of default php.ini settings that apply to both CLI and FastCGI
+#   mode.
+#
+# [*fcgi_settings*]
+#   A hash of php.ini settings for that only apply to FastCGI mode.
 #
 # [*logroot*]
 #   Parent directory to write log files to. An 'hhvm' subdirectory will be
@@ -12,6 +45,8 @@
 #   /vagrant/logs)
 #
 class hhvm (
+  $common_settings,
+  $fcgi_settings,
   $logroot,
 ) {
     include ::apache
@@ -29,18 +64,21 @@ class hhvm (
         require     => Package['hhvm'],
     }
 
-    file { '/etc/hhvm':
+    file { ['/etc/hhvm', '/etc/hhvm/fcgi']:
         ensure => directory,
+    }
+
+    file { '/etc/hhvm/php.ini':
+        content => php_ini($common_settings),
+    }
+
+    file { '/etc/hhvm/fcgi/php.ini':
+        content => php_ini($common_settings, $fcgi_settings),
+        notify  => Service['hhvm'],
     }
 
     file { '/etc/hhvm/config.hdf':
         content => template('hhvm/config.hdf.erb'),
-        require => Package['hhvm'],
-        notify  => Service['hhvm'],
-    }
-
-    file { '/etc/hhvm/php.ini':
-        content => template('hhvm/php.ini.erb'),
         require => Package['hhvm'],
         notify  => Service['hhvm'],
     }
