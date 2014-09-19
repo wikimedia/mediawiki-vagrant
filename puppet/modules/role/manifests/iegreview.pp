80454259d97052c6d588e81f12e6cf6cc41166d1
Bryan Davis <bd808@bd808.com>
2014-09-19 10:52:06 -0600

    Add role::iegreview
    Add a module and role for provisioning the IEG grant review application.
    
    Change-Id: I2ec6beed7be9652bee0dcad51da0b24aaf29250e

---
+++ puppet/modules/role/manifests/iegreview.pp
@@ -0,0 +1,10 @@
+# == Class: role::iegreview
+# Provisions the IEG grant review application.
+#
+# *Note*: The application is provisioned using an Apache named virtual host.
+# Once the role is enabled and provisioned use the URL
+# http://iegreview.local.wmftest.net:8080/ to access the site.
+class role::iegreview {
+    include ::iegreview
+    include ::smtplog
+}
