40a4bc050553899a31fa2c4fd9ae444a8b226ae0
Max Semenik <maxsem.wiki@gmail.com>
2014-10-07 13:42:49 -0700

    SiteMatrix: return proper URLs for wikis named <langcode>wiki
    Change-Id: I9c620a04644b2ac64d3282647bdb9ba4b26caeea

--- puppet/modules/role/manifests/sitematrix.pp
+++ puppet/modules/role/manifests/sitematrix.pp
@@ -1,5 +1,8 @@
 # == Class: role::sitematrix
 # The sitematrix extension adds a special page with a matrix of all sites
 class role::sitematrix {
-    mediawiki::extension { 'SiteMatrix': }
+    mediawiki::extension { 'SiteMatrix':
+        # TODO: DB postfixes other than 'wiki' aren't supported elsewhere
+        settings => "\$wgSiteMatrixSites['wiki']['host'] = 'www${mediawiki::multiwiki::base_domain}${::port_fragment}';",
+    }
 }
