2c1dc87ec7c01873c180595aea69490f57a45b81
Max Semenik <maxsem.wiki@gmail.com>
2014-10-21 12:46:22 -0700

    Fix WikiGrok
    Change-Id: I8851c3d53864d5b3267b7614bfd0e46e82f4e86a

--- puppet/modules/role/manifests/wikigrok.pp
+++ puppet/modules/role/manifests/wikigrok.pp
@@ -1,7 +1,8 @@
 # == Class: role::wikigrok
 # Creates an easy way to fill in missing Wikidata information
 class role::wikigrok {
-    include ::role::wikibase
+    include ::role::wikidata
+    include ::role::mobilefrontend
 
     mediawiki::extension { 'WikiGrok': }
 }
