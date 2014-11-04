c7cafe2057aa528fca13d5e8b4bfbcd4b4d7a8be
Brad Jorsch <bjorsch@wikimedia.org>
2014-11-04 12:53:20 -0500

    Add role for SecurePoll
    Change-Id: I5831daaf0ddb55bc3ba55110053a43f87dfe391d

---
+++ puppet/modules/role/manifests/securepoll.pp
@@ -0,0 +1,18 @@
+# == Class: role::securepoll
+# This role installs the SecurePoll extension and creates an additional wiki:
+#
+# _vote.wiki.local.wmftest.net_::
+#   Wiki where voting happens. Sysops here can create polls.
+#
+class role::securepoll {
+    require ::role::mediawiki
+
+    require_package('gnupg')
+
+    mediawiki::extension { 'SecurePoll':
+        needs_update => true,
+        settings     => template('role/securepoll.php.erb'),
+    }
+
+    mediawiki::wiki { 'vote': }
+}
