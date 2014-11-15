781b8f55645f575cd2d65b3cbb6a18d6a8af72ff
Bryan Davis <bd808@wikimedia.org>
2014-11-15 09:34:46 -0700

    Add rsyslog module
    Import the rsyslog module from operations/puppet.
    
    Change-Id: I842f72e074f2cd5bd470874aa513eee95a063aef

---
+++ puppet/modules/rsyslog/manifests/init.pp
@@ -0,0 +1,30 @@
+# == Class: rsyslog
+#
+# rsyslogd is a full-featured kernel logging daemon. It is the default
+# syslogd implementation on Debian systems.
+#
+class rsyslog {
+    package { 'rsyslog':
+        ensure => present,
+    }
+
+    file { '/etc/rsyslog.d':
+        ensure  => directory,
+        source  => 'puppet:///modules/rsyslog/rsyslog.d-empty',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
+        recurse => true,
+        purge   => true,
+        force   => true,
+        ignore  => '50-default.conf',
+        require => Package['rsyslog'],
+        notify  => Service['rsyslog'],
+    }
+
+    service { 'rsyslog':
+        ensure    => running,
+        provider  => 'upstart',
+        require   => Package['rsyslog'],
+    }
+}
