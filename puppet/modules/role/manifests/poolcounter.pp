8959f7f7a36dfc8b5ea62f8f58f710163e40748d
Nik Everett <neverett@wikimedia.org>
2014-12-02 12:23:17 -0500

    Add role to install and use pool counter
    Configures article views to go through the pool counter.
    
    Change-Id: Iddb676d05fa6af88c85a08f0b8f66978f2fb8f5a

---
+++ puppet/modules/role/manifests/poolcounter.pp
@@ -0,0 +1,26 @@
+# == Class: role::poolcounter
+#
+# Installs pool counter service and configures wikis to use it for article
+# views.
+#
+class role::poolcounter {
+    require_package('poolcounter')
+
+    service { 'poolcounter':
+        ensure  => 'running',
+        require => Package['poolcounter'],
+    }
+
+    mediawiki::extension { 'PoolCounter':
+        priority => $LOAD_EARLY,
+        settings => [
+            '$wgPoolCountClientConf["servers"][] = "127.0.0.1";',
+            '$wgPoolCountClientConf["timeout"] = 0.5;',
+            '$wgPoolCounterConf["ArticleView"]["class"] = "PoolCounter_Client";',
+            '$wgPoolCounterConf["ArticleView"]["timeout"] = 15;',
+            '$wgPoolCounterConf["ArticleView"]["workers"] = 2;',
+            '$wgPoolCounterConf["ArticleView"]["maxqueue"] = 100;',
+        ],
+        require  => Service['poolcounter'],
+    }
+}
