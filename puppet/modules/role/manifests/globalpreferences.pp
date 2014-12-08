0c0a6eda3034f5f1d3542afb397c736b6d4110df
Prateek Saxena <prtksxna@gmail.com>
2014-12-08 06:57:32 +0530

    Add role for GlobalPreferences
    Bug: T76835
    Change-Id: I457d978f2ca773714831d7e0074f0f8b7332c439

---
+++ puppet/modules/role/manifests/globalpreferences.pp
@@ -0,0 +1,24 @@
+# == Class: role::globalpreferences
+# Configures the GlobalPreferences extension
+class role::globalpreferences {
+    include ::role::centralauth
+
+    $shared_db = $::role::centralauth::shared_db
+
+    mediawiki::extension { 'GlobalPreferences':
+        needs_update => true,
+        settings     => {
+            wgGlobalPreferencesDB => $shared_db
+        },
+    }
+
+    mysql::sql { 'Create global_preferences table':
+        sql     => "USE ${shared_db}; SOURCE ${::mediawiki::dir}/extensions/GlobalPreferences/schema.sql;",
+        unless  => "SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE table_schema = '${shared_db}' AND table_name = 'global_preferences';",
+        require => [
+            Mysql::Db[$shared_db],
+            Mediawiki::Extension['GlobalPreferences']
+        ],
+        before  => Exec['update_all_databases'],
+    }
+}
