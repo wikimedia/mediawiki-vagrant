9cd92d68af3269bbd046848e9c9b985101567a87
Gergő Tisza <gtisza@wikimedia.org>
2014-12-23 18:28:56 +0000

    Add Vagrant role for Sentry (server + MW extension)
    Bug: T84957
    Change-Id: Ibfd70d3df9aee99f617013e369965fb5f4227bbd

---
+++ puppet/modules/role/manifests/sentry.pp
@@ -0,0 +1,20 @@
+# == Class: role::sentry
+# Installs a Sentry instance which collects errors from your wiki
+#
+# [*dsn*]
+#   The Sentry DSN to use, see
+#   http://raven.readthedocs.org/en/latest/config/#the-sentry-dsn
+#
+class role::sentry (
+    $dsn,
+) {
+    include ::sentry
+
+    mediawiki::extension { 'Sentry':
+        settings => [
+            # OMG that's ugly. Did not find a better way to reuse
+            # the output of a script.
+            "\$wgSentryDsn = file_get_contents('${dsn}');",
+        ],
+    }
+}
---
+++ puppet/modules/virtualenv/manifests/environment.pp
@@ -0,0 +1,62 @@
+# == Class virtualenv
+# Helper class to install python packages via virtualenv.
+# Will create a virtualenv directory with the given packages.
+#
+# This is a nasty hack which does not use package providers
+# as puppet does not suppose using those with virtualenv:
+# https://tickets.puppetlabs.com/browse/PUP-1062
+#
+# === Parameters
+#
+# [*ensure*]
+#   'present' to create the environment, 'absent' to delete
+#   it (along with all contents, whether puppet-managed or not)
+#
+# [*dir*]
+#   The directory where the virtual environment should be.
+#   Will be created if it does not exist.
+#
+# [*packages*]
+#   An array of pip packages to install. Must not be empty.
+#
+# [*owner*]
+#   User owner of the environment directory and created files.
+#
+# [*group*]
+#   Group owner of the environment directory and created files.
+#
+define virtualenv::environment (
+    $packages,
+    $dir    = $title,
+    $ensure = 'present',
+    $owner  = 'root',
+    $group  = 'root',
+) {
+    require virtualenv
+
+    if $ensure == 'present' {
+        file { $dir:
+            ensure => directory,
+            owner  => $owner,
+            group  => $group,
+        }
+
+        exec { "virtualenv-${dir}":
+            command => join(["virtualenv . && ./bin/pip install '", join($packages, "' '"), "'"]),
+            cwd     => $dir,
+            creates => "${dir}/lib",
+            user    => $owner,
+            group   => $group,
+            require => File[$dir],
+        }
+    } elsif $ensure == 'absent' {
+        file { $dir:
+            ensure  => absent,
+            force   => true,
+            recurse => true,
+            purge   => true,
+        }
+    }
+}
+
+
