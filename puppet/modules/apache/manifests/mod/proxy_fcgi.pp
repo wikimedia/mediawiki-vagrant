a822d273422a4362c9d8ada11a805b78977b6c3b
Dan Duvall <dduvall@wikimedia.org>
2015-01-08 13:24:59 -0800

    OAuth/FCGI header fix backported from production
    Backported production fixes that pass headers normally removed by
    mod_proxy_fcgi and needed by OAuth via environment variables. See
    I56a59db1 and I232138e6 for the related production changes.
    
    Bug: T76022
    Change-Id: I1b593faaa7d7a01c30901ba9f584f197dad36909

--- puppet/modules/apache/manifests/mod/proxy_fcgi.pp
+++ puppet/modules/apache/manifests/mod/proxy_fcgi.pp
@@ -3,5 +3,10 @@
 class apache::mod::proxy_fcgi {
     if versioncmp($::lsbdistrelease, '13.10') >= 0 {
         apache::mod_conf { 'proxy_fcgi': }
+
+        apache::conf { 'fcgi_headers':
+            source   => 'puppet:///modules/apache/conf/fcgi_headers.conf',
+            priority => 0,
+        }
     }
 }
