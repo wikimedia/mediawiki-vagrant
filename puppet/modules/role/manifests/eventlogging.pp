869476cf8085182448c76c5cda1dc27e88f2d428
Marcel Ruiz Forns <mforns@wikimedia.org>
2015-01-13 23:44:26 +0100

    Route eventlogging-devserver from 8080 to 8100
    To be able to execute eventlogging-devserver inside vagrant,
    this fix makes EventLogging client request through 8080 port
    and configures apache to reverse-proxy 8080 to 8100,
    the port eventlogging-devserver should be listening to.
    
    Bug: T86067
    Change-Id: Iacde21353d112d212b84cb022abbf1899e562322

--- puppet/modules/role/manifests/eventlogging.pp
+++ puppet/modules/role/manifests/eventlogging.pp
@@ -3,11 +3,17 @@
 # events are validated against production schemas but logged locally.
 class role::eventlogging {
     include ::role::geshi
+    include ::apache::mod::proxy
+    include ::apache::mod::proxy_http
+    include ::apache::mod::headers
+
+    # Define a 'ELDevServer' parameter for Apache <IfDefine> checks.
+    apache::def { 'ELDevServer': }
 
     mediawiki::extension { 'EventLogging':
         priority => $::LOAD_EARLY,
         settings => {
-            wgEventLoggingBaseUri => '//localhost:8100/event.gif',
+            wgEventLoggingBaseUri => '//localhost:8080/event.gif',
             wgEventLoggingFile    => '/vagrant/logs/eventlogging.log',
         }
     }
