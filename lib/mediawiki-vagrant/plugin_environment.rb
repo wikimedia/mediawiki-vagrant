c85a5978453e1aed40dd94697c6761eaee6c5582
Bryan Davis <bd808@wikimedia.org>
2015-01-25 10:28:06 -0800

    plugin: Optional display of only enabled roles
    Add new options to `vagrant roles list`:
    * `--enabled`: only show enabled roles
    * `--onecol`: show output in a single column without headers/footers
    * `--quiet`: suppress header and footer information
    
    Among other things this enables the `vagrant roles list -e -1`
    variation that will list all enabled roles in a single column of output
    with no report header or footer. This output is suitable for use in
    a shell pipeline that wants to process a list of enabled roles.
    
    The command assumes `--quiet` and `--onecol` as the default for output
    to non-interactive terminals (i.e. piped or redirected).
    
    Change-Id: I19160e9339cd8e10000df5b0ea9e6b544c7b9263

--- lib/mediawiki-vagrant/plugin_environment.rb
+++ lib/mediawiki-vagrant/plugin_environment.rb
@@ -6,5 +6,13 @@ module MediaWikiVagrant
       super
       @mwv = Environment.new(@env.root_path || @env.cwd)
     end
+
+    # Whether we're sending output to an interactive terminal.
+    #
+    # @return [true, false]
+    #
+    def interactive_out?
+      $stdout.tty?
+    end
   end
 end
--- lib/mediawiki-vagrant/roles/list.rb
+++ lib/mediawiki-vagrant/roles/list.rb
@@ -10,33 +10,90 @@ module MediaWikiVagrant
       end
 
       def execute
-        opts = OptionParser.new do |o|
-          o.banner = 'Usage: vagrant roles list [-h]'
+        opts = {}
+        optparse = OptionParser.new do |o|
+          o.banner = 'Usage: vagrant roles list [options]'
           o.separator ''
           o.separator '  List available roles.'
           o.separator ''
+          o.separator 'Options:'
+
+          opts[:enabled] = false
+          o.on('-e', '--enabled', 'Only show enabled roles') do
+            opts[:enabled] = true
+          end
+
+          opts[:single_col] = !interactive_out?
+          o.on('-1', '--onecol', 'Single column output without header/footer') do
+            opts[:single_col] = true
+            opts[:verbose] = false
+          end
+
+          opts[:verbose] = interactive_out?
+          o.on('-q', '--quiet', 'Suppress output of headers and footers') do
+            opts[:verbose] = false
+          end
+
+          o.on_tail('-h', '--help', 'Display this help screen') do
+            @env.ui.info o
+            exit
+          end
         end
 
-        argv = parse_options(opts)
-        return if !argv
+        begin
+          optparse.parse!
+        rescue OptionParser::ParseError => e
+          @env.ui.error e
+          @env.ui.info optparse
+          exit
+        end
 
-        @env.ui.info "Available roles:\n"
+        if opts[:enabled]
+          show_enabled(opts)
+        else
+          show_all(opts)
+        end
+      end
+
+      def show_enabled(opts)
+        @env.ui.info "Enabled roles:\n" if opts[:verbose]
+        roles = @mwv.roles_enabled
+        if opts[:single_col]
+          roles.each { |x| @env.ui.info x }
+        else
+          print_cols(roles)
+        end
+      end
+
+      def show_all(opts)
+        @env.ui.info "Available roles:\n" if opts[:verbose]
         enabled = @mwv.roles_enabled
 
         roles = @mwv.roles_available.sort.map { |role|
           prefix = enabled.include?(role) ? '*' : ' '
           "#{prefix} #{role}"
         }
+
+        if opts[:single_col]
+          roles.each { |x| @env.ui.info x }
+        else
+          print_cols(roles)
+        end
+
+        if opts[:verbose]
+          @env.ui.info "\nRoles marked with '*' are enabled."
+          @env.ui.info 'Note that roles enabled by dependency are not marked.'
+          @env.ui.info 'Use `vagrant roles enable` & `vagrant roles disable` to customize.'
+        end
+
+        0
+      end
+
+      def print_cols(roles)
         col, *cols = roles.each_slice((roles.size/3.0).ceil).to_a
         col.zip(*cols) { |a,b,c|
           @env.ui.info sprintf("%-26s %-26s %-26s", a, b, c)
         }
-
-        @env.ui.info "\nRoles marked with '*' are enabled."
-        @env.ui.info 'Note that roles enabled by dependency are not marked.'
-        @env.ui.info 'Use `vagrant roles enable` & `vagrant roles disable` to customize.'
-
-        0
       end
     end
   end
