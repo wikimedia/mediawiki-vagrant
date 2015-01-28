1fedfe0396fba40688cb583de471b9e10bcafbd3
Brion Vibber <brion@pobox.com>
2015-01-28 12:46:50 -0800

    Add MediaWiki-Vagrant role for 'Score' extension
    Installs GNU LilyPond as a system dependency.
    
    Bug: T87794
    Change-Id: I4f0ecd8c8f525280fe156439d1c5aae8823470a7

---
+++ puppet/modules/role/manifests/score.pp
@@ -0,0 +1,17 @@
+# == Class: role::score
+# This role provisions the
+# Score[https://www.mediawiki.org/wiki/Extension:Score] extension, which
+# renders musical notation via GNU LilyPond.
+#
+class role::score {
+    include ::role::multimedia
+
+    require_package('lilypond')
+
+    mediawiki::extension { 'Score':
+        needs_update => true,
+        require      => [
+            Package['lilypond'],
+        ],
+    }
+}
