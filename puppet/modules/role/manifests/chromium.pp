a145012c88c79f815d8f62c1e881cbb02ca35293
Timo Tijhof <krinklemail@gmail.com>
2015-03-08 18:01:00 +0100

    Remove (broken) 'chromium' role
    Follows-up ce420a12c24ee5e.
    
    Bug: T91933
    Change-Id: I197fb534c2acd7684decdb007bf3083d201361f7

--- puppet/modules/role/manifests/chromium.pp
+++
@@ -1,8 +0,0 @@
-# == Class: role::chromium
-#
-# Chromium is the open source web browser project from which Google
-# Chrome draws its source code. This role provisions a browser instance
-# that runs in headless mode and that can be automated by various tools.
-class role::chromium {
-    include ::chromium
-}
