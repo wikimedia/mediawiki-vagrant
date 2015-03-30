1fa1de1eb1a35fb439859af992622649e2811b4a
Max Semenik <maxsem.wiki@gmail.com>
2015-03-30 09:54:06 -0700

    Role for Disambiguator
    Change-Id: I82521b1f383691f10c410908fd36e4457f50e849

---
+++ puppet/modules/role/manifests/disambiguator.pp
@@ -0,0 +1,6 @@
+# == Class: role::disambiguator
+# The Disambiguator extension provides functions to programmatically mark disambiguation
+# pages as such and keep a list of them
+class role::disambiguator {
+    mediawiki::extension { 'Disambiguator': }
+}
