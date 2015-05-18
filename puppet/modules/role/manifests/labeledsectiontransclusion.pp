755cc602950bde038de1bc35aceb10468fabbcb8
Chad Horohoe <chadh@wikimedia.org>
2015-05-18 04:27:52 -0700

    Fix spelling of labledsectiontransclusion
    Rename the file (authored by chadh in the summer of 2014) and the terms
    in it.
    
    Bug: T99514
    Change-Id: I8d3e5f5617028e803a63cdee897cb4f9ef3568f0

---
+++ puppet/modules/role/manifests/labeledsectiontransclusion.pp
@@ -0,0 +1,5 @@
+# == Class: role::labeledsectiontransclusion
+# Configures LabeledSectionTransclusion, an extension to let you include parts of a page
+class role::labeledsectiontransclusion {
+    mediawiki::extension { 'LabeledSectionTransclusion': }
+}
--- puppet/modules/role/manifests/labledsectiontransclusion.pp
+++
@@ -1,5 +0,0 @@
-# == Class: role::labledsectiontransclusion
-# Configures LabledSectionTransclusion, an extension to let you include parts of a page
-class role::labledsectiontransclusion {
-    mediawiki::extension { 'LabledSectionTransclusion': }
-}
