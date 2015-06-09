26b833b9c3c193770385a20d343e110d68470333
Dan Duvall <dduvall@wikimedia.org>
2015-06-09 11:32:03 -0700

    Add role for hacking on Raita
    This role clones the integration/raita repo, sets up Elasticsearch, and
    imports mappings and some test data for development.
    
    Change-Id: I55f4891040f6733ad86aa1a467135cfa2fa5ea24

---
+++ puppet/modules/role/manifests/raita.pp
@@ -0,0 +1,60 @@
+# == Class: role::raita
+#
+# Raita is a dashboard built on Elasticsearch for visualizing and diagnosing
+# browser test failures. The default URL for accessing the running dashboard
+# will be http://raita.local.wmftest.net:8080/ though this may change
+# according to your forwarded HTTP port or Hiera configuration.
+#
+# [*dir*]
+#   Repository clone directory
+#
+# [*vhost_name*]
+#   Apache virtual host name
+#
+class role::raita(
+    $dir,
+    $vhost_name,
+) {
+    include ::elasticsearch
+    include ::apache::mod::headers
+    include ::apache::mod::proxy
+    include ::apache::mod::proxy_http
+
+    require_package('nodejs')
+
+    $index_url = 'http://127.0.0.1:9200/raita'
+    $index = template('role/raita/index.json.erb')
+
+    git::clone { 'integration/raita':
+        directory => $dir,
+        require   => Package['nodejs'],
+    }
+
+    exec { 'raita elasticsearch index':
+        command => "curl -X PUT '${index_url}/' -d '${index}'",
+        unless  => "curl -sf '${index_url}/_settings' 2> /dev/null",
+        require => [Class['elasticsearch'], Git::Clone['integration/raita']],
+    }
+
+    exec { 'raita elasticsearch mappings':
+        command => "/usr/bin/nodejs scripts/mappings.js import '${index_url}'",
+        unless  => "/usr/bin/nodejs scripts/mappings.js check '${index_url}'",
+        cwd     => $dir,
+        require => Exec['raita elasticsearch index'],
+    }
+
+    file { '/tmp/raita-data.json':
+        source => 'puppet:///modules/role/raita/data.json',
+    }
+
+    exec { 'raita import elasticsearch data':
+        command   => "curl -X POST '${index_url}/_bulk' --data-binary @/tmp/raita-data.json",
+        onlyif    => "curl -sf '${index_url}/_count' | grep -q '\"count\":0'",
+        require   => Exec['raita elasticsearch mappings'],
+        subscribe => File['/tmp/raita-data.json'],
+    }
+
+    apache::site { $vhost_name:
+        content => template('role/raita/apache.conf.erb'),
+    }
+}
