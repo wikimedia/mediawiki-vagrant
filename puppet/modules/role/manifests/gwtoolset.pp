5e2c83ef5747810defa85c8d4e69c35787260fe3
Tim Landscheidt <tim@tim-landscheidt.de>
2015-06-27 21:43:59 +0000

    role::gwtoolset: Fix sentinel pages for import_dump
    Change-Id: Ief93789802a6b451b95b11c76ce558913cf80240

--- puppet/modules/role/manifests/gwtoolset.pp
+++ puppet/modules/role/manifests/gwtoolset.pp
@@ -35,86 +35,86 @@ class role::gwtoolset {
 
     mediawiki::import::dump { 'mediawiki_common_css':
         xml_dump           => '/vagrant/puppet/modules/role/files/gwtoolset/mediawiki/Common.css.xml',
-        dump_sentinel_page => 'Testwiki:wiki/Mediawiki:Common.css',
+        dump_sentinel_page => 'Mediawiki:Common.css',
     }
 
     mediawiki::import::dump { 'mediawiki_editsection_brackets':
         xml_dump           => '/vagrant/puppet/modules/role/files/gwtoolset/mediawiki/Editsection-brackets.xml',
-        dump_sentinel_page => 'Testwiki:wiki/Mediawiki:Editsection-brackets',
+        dump_sentinel_page => 'Mediawiki:Editsection-brackets',
     }
 
     mediawiki::import::dump { 'mediawiki_lang':
         xml_dump           => '/vagrant/puppet/modules/role/files/gwtoolset/mediawiki/Lang.xml',
-        dump_sentinel_page => 'Testwiki:wiki/Mediawiki:Lang',
+        dump_sentinel_page => 'Mediawiki:Lang',
     }
 
     mediawiki::import::dump { 'template_de':
         xml_dump           => '/vagrant/puppet/modules/role/files/gwtoolset/template/De.xml',
-        dump_sentinel_page => 'Testwiki:wiki/Template:De',
+        dump_sentinel_page => 'Template:De',
     }
 
     mediawiki::import::dump { 'template_en':
         xml_dump           => '/vagrant/puppet/modules/role/files/gwtoolset/template/En.xml',
-        dump_sentinel_page => 'Testwiki:wiki/Template:En',
+        dump_sentinel_page => 'Template:En',
     }
 
     mediawiki::import::dump { 'template_fr':
         xml_dump           => '/vagrant/puppet/modules/role/files/gwtoolset/template/Fr.xml',
-        dump_sentinel_page => 'Testwiki:wiki/Template:Fr',
+        dump_sentinel_page => 'Template:Fr',
     }
 
     mediawiki::import::dump { 'template_nl':
         xml_dump           => '/vagrant/puppet/modules/role/files/gwtoolset/template/Nl.xml',
-        dump_sentinel_page => 'Testwiki:wiki/Template:Nl',
+        dump_sentinel_page => 'Template:Nl',
     }
 
     mediawiki::import::dump { 'module_templatepar':
         xml_dump           => '/vagrant/puppet/modules/role/files/gwtoolset/module/TemplatePar.xml',
-        dump_sentinel_page => 'Testwiki:wiki/Module:TemplatePar',
+        dump_sentinel_page => 'Module:TemplatePar',
     }
 
     mediawiki::import::dump { 'template_pd_old':
         xml_dump           => '/vagrant/puppet/modules/role/files/gwtoolset/template/PD-old.xml',
-        dump_sentinel_page => 'Testwiki:wiki/Template:PD-old',
+        dump_sentinel_page => 'Template:PD-old',
     }
 
     mediawiki::import::dump { 'template_pd_us':
         xml_dump           => '/vagrant/puppet/modules/role/files/gwtoolset/template/PD-US.xml',
-        dump_sentinel_page => 'Testwiki:wiki/Template:PD-US',
+        dump_sentinel_page => 'Template:PD-US',
     }
 
     mediawiki::import::dump { 'template_cc_by_30':
         xml_dump           => '/vagrant/puppet/modules/role/files/gwtoolset/template/Cc-by-3.0.xml',
-        dump_sentinel_page => 'Testwiki:wiki/Template:Cc-by-3.0',
+        dump_sentinel_page => 'Template:Cc-by-3.0',
     }
 
     mediawiki::import::dump { 'template_cc_by_sa_30':
         xml_dump           => '/vagrant/puppet/modules/role/files/gwtoolset/template/Cc-by-sa-3.0.xml',
-        dump_sentinel_page => 'Testwiki:wiki/Template:Cc-by-sa-3.0',
+        dump_sentinel_page => 'Template:Cc-by-sa-3.0',
     }
 
     mediawiki::import::dump { 'template_uploaded_with_gwtoolset':
         xml_dump           => '/vagrant/puppet/modules/role/files/gwtoolset/template/Uploaded_with_GWToolset.xml',
-        dump_sentinel_page => 'Testwiki:wiki/Template:Uploaded_with_GWToolset',
+        dump_sentinel_page => 'Template:Uploaded_with_GWToolset',
     }
 
     mediawiki::import::dump { 'template_artwork':
         xml_dump           => '/vagrant/puppet/modules/role/files/gwtoolset/template/Artwork.xml',
-        dump_sentinel_page => 'Testwiki:wiki/Template:Artwork',
+        dump_sentinel_page => 'Template:Artwork',
     }
 
     mediawiki::import::dump { 'mapping_beeld_en_geluid':
         xml_dump           => '/vagrant/puppet/modules/role/files/gwtoolset/mapping/GWToolset-Metadata_Mappings-Dan-nl-Beeld-en-Geluid.xml',
-        dump_sentinel_page => 'Testwiki:wiki/GWToolset:Metadata_Mappings/Dan-nl/Beeld-en-Geluid.json',
+        dump_sentinel_page => 'GWToolset:Metadata_Mappings/Dan-nl/Beeld-en-Geluid.json',
     }
 
     mediawiki::import::dump { 'mapping_kb':
         xml_dump           => '/vagrant/puppet/modules/role/files/gwtoolset/mapping/GWToolset-Metadata_Mappings-Dan-nl-KB_Centsprenten.xml',
-        dump_sentinel_page => 'Testwiki:wiki/GWToolset:Metadata_Mappings/Dan-nl/KB_Centsprenten.json',
+        dump_sentinel_page => 'GWToolset:Metadata_Mappings/Dan-nl/KB_Centsprenten.json',
     }
 
     mediawiki::import::dump { 'mapping_rijksmuseum':
         xml_dump           => '/vagrant/puppet/modules/role/files/gwtoolset/mapping/GWToolset-Metadata_Mappings-Dan-nl-Rijksmuseum.xml',
-        dump_sentinel_page => 'Testwiki:wiki/GWToolset:Metadata_Mappings/Dan-nl/Rijksmuseum.json',
+        dump_sentinel_page => 'GWToolset:Metadata_Mappings/Dan-nl/Rijksmuseum.json',
     }
 }
