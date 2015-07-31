0f3c57804b876a033370b61fee959d37fe617826
Kunal Mehta <legoktm@gmail.com>
2015-07-31 01:30:18 -0700

    Use bundled version of Pygments
    The SyntaxHighlight_GeSHi repository has a copy of pygments bundled with
    it, use it since it will be the same version of pygments that is
    deployed.
    
    Bug: T105338
    Change-Id: Ib9d87b4c356477111c5528c04540aae0629d5867

--- puppet/modules/role/manifests/geshi.pp
+++ puppet/modules/role/manifests/geshi.pp
@@ -1,12 +1,7 @@
 # == Class: role::geshi
 # Configures SyntaxHighlight_GeSHi, an extension for syntax-highlighting
 class role::geshi {
-    require_package('python-pygments')
-
     mediawiki::extension { 'SyntaxHighlight_GeSHi':
         composer => true,
-        settings => {
-            wgPygmentizePath => '/usr/bin/pygmentize',
-        },
     }
 }
