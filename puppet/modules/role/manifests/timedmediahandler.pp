d94d0eefe7de3b8c00a5706452c7731ba8cb5707
Brion Vibber <brion@pobox.com>
2015-08-07 04:40:22 -0700

    Switch MW-Vagrant TMH to use ffmpeg instead of avconv
    Works around horrible bugs in avconv on Ubuntu 15.04, and
    lets us consume and produce VP9 and Opus now that we have
    an updated ffmpeg.
    
    Bug: T106612
    Change-Id: I247d13c7018a72d881dde7628644e4758739c35e

--- puppet/modules/role/manifests/timedmediahandler.pp
+++ puppet/modules/role/manifests/timedmediahandler.pp
@@ -5,17 +5,18 @@ class role::timedmediahandler {
     include ::role::multimedia
 
     require_package('ffmpeg2theora')
-    require_package('libav-tools')
+    require_package('ffmpeg')
 
     mediawiki::extension { 'MwEmbedSupport': }
 
     mediawiki::extension { 'TimedMediaHandler':
         settings     => {
             wgWaitTimeForTranscodeReset => 1,
+            wgFFmpegLocation            => '/usr/bin/ffmpeg'
         },
         needs_update => true,
         require      => [
-            Package['libav-tools', 'ffmpeg2theora'],
+            Package['ffmpeg', 'ffmpeg2theora'],
             Mediawiki::Extension['MwEmbedSupport']
         ],
     }
