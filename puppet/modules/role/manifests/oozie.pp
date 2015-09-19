6fc10e58c315a12a0e02587c7883965d35f05817
Erik Bernhardson <ebernhardson@wikimedia.org>
2015-09-19 11:03:47 -0700

    Require mysql before initializing oozie database
    When setting up the analytics role it bailed as unable to connect
    to mysql. The passwords were set just a few lines after the failure.
    
    The analytics role still doesn't seem to work, it fails later unable
    to start the hadoop-yarn-resourcemanager service, but this patch gets
    it moving along at least.
    
    Change-Id: I81140fc449e15f6f19c251a6f7440faf9522f22a

--- puppet/modules/role/manifests/oozie.pp
+++ puppet/modules/role/manifests/oozie.pp
@@ -12,4 +12,7 @@ class role::oozie {
     # Make sure HDFS is totally ready before the CDH
     # module tries to create this directory.
     Exec['wait_for_hdfs'] -> Cdh::Hadoop::Directory['/user/oozie']
+
+    # Make sure mysql is ready before cdh trys to setup it's db
+    Exec['set_mysql_password'] -> Exec['oozie_mysql_create_database']
 }
