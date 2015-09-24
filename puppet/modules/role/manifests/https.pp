a18cff3f61721cba42db8781421e3a239308d58d
Gergő Tisza <tgr.huwiki@gmail.com>
2015-09-24 16:33:03 -0700

    Fix directory name for https role nginx config
    Change-Id: Ie35876dd96adbe737bd9e06c2193b9fb8b14def2

--- puppet/modules/role/manifests/https.pp
+++ puppet/modules/role/manifests/https.pp
@@ -11,7 +11,7 @@ class role::https {
     }
 
     nginx::site { 'devwiki':
-        source => 'puppet:///modules/role/nginx/nginx.conf',
+        source => 'puppet:///modules/role/https/nginx.conf',
         notify => Service['nginx'],
     }
 
