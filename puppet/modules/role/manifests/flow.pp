de6e57670ecb5efd84ce9fccf709a76f84e0e21d
Matthew Flaschen <mflaschen@wikimedia.org>
2015-10-27 18:58:44 -0400

    Add users and groups for Flow browser tests
    Bug: T115515
    Change-Id: If366eb5d5b0134eff56417df6686a5fbcb053b67

--- puppet/modules/role/manifests/flow.pp
+++ puppet/modules/role/manifests/flow.pp
@@ -16,6 +16,31 @@ class role::flow {
         browser_tests => '.',
     }
 
+    mediawiki::group { 'flow-creator':
+      wiki              => $::mediawiki::wiki_name,
+      group_name        => 'flow-creator',
+      grant_permissions => [
+        'flow-create-board',
+      ],
+    }
+
+    mediawiki::user { 'Selenium_privileged_Flow_user':
+        username => 'Selenium Flow user',
+        password => $::mediawiki::admin_pass,
+        wiki     => $::mediawiki::db_name,
+        groups   => [
+            'sysop',
+            'suppress',
+            'flow-creator',
+        ],
+    }
+
+    mediawiki::user { 'Selenium_regular_Flow_user':
+        username => 'Selenium Flow user 2',
+        password => $::mediawiki::admin_pass,
+        wiki     => $::mediawiki::db_name,
+    }
+
     file { '/etc/logrotate.d/mediawiki_Flow':
         source => 'puppet:///modules/role/flow/logrotate.d-mediawiki-Flow',
         owner  => 'root',
