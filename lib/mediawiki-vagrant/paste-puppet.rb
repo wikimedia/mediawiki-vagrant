9d2c4b7acbbab162a9bf8adbd5c5c9b50eeef40d
Željko Filipin <zeljko.filipin@gmail.com>
2015-11-02 13:30:57 +0100

    Fixed Style/RedundantBegin RuboCop offense
    Bug: T106220
    Change-Id: Id0e6cd635b5d67e8c851de890c5bc983fa785a09

--- lib/mediawiki-vagrant/paste-puppet.rb
+++ lib/mediawiki-vagrant/paste-puppet.rb
@@ -14,23 +14,23 @@ module MediaWikiVagrant
     end
 
     def execute
-      begin
-        res = Net::HTTP.post_form URL, content: latest_logfile.read
-        raise unless res.value.nil? && res.body =~ /^"[^"]+"$/
-      rescue RuntimeError
-        @env.ui.error "Unexpected response from #{URL}."
-        1
-      rescue TypeError
-        @env.ui.error 'No Puppet log files found.'
-        1
-      rescue SocketError, Net::HTTPExceptions
-        @env.ui.error "Unable to connect to #{URL}."
-        1
-      else
-        @env.ui.success "HTTP #{res.code} #{res.msg}"
-        @env.ui.info res.body[1...-1]
-        0
-      end
+
+      res = Net::HTTP.post_form URL, content: latest_logfile.read
+      raise unless res.value.nil? && res.body =~ /^"[^"]+"$/
+    rescue RuntimeError
+      @env.ui.error "Unexpected response from #{URL}."
+      1
+    rescue TypeError
+      @env.ui.error 'No Puppet log files found.'
+      1
+    rescue SocketError, Net::HTTPExceptions
+      @env.ui.error "Unable to connect to #{URL}."
+      1
+    else
+      @env.ui.success "HTTP #{res.code} #{res.msg}"
+      @env.ui.info res.body[1...-1]
+      0
+
     end
 
     private
