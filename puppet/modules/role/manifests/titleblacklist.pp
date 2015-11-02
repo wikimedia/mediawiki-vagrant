fa9b8ee217ca285982fc815de1b0eb197db5650e
Gergő Tisza <tgr.huwiki@gmail.com>
2015-11-02 14:40:55 -0800

    Add titleblacklist role
    Change-Id: Ief70ecc4cec46849ccad9604d1c29e58c476a6ce

---
+++ puppet/modules/role/manifests/titleblacklist.pp
@@ -0,0 +1,27 @@
+# == Class: role::titleblacklist
+# Provisions the TitleBlacklist extension which allows wiki
+# administrators to block the creation, movement and upload of
+# pages, the title of which matches one or more regular
+# expressions, as well as blocking creation of accounts with matching usernames.
+#
+class role::titleblacklist {
+    mediawiki::extension { 'TitleBlacklist':
+        settings => {
+            wgTitleBlacklistSources => [
+                {
+                    type => 'localpage',
+                    src  => 'MediaWiki:Titleblacklist',
+                },
+                {
+                    type => 'url',
+                    src  => 'https://meta.wikimedia.org/w/index.php?title=Title_blacklist&action=raw',
+                },
+            ],
+        },
+    }
+
+    mediawiki::import::text { 'VagrantRoleTitleBlacklist':
+        source => 'puppet:///modules/role/titleblacklist/VagrantRoleTitleBlacklist.wiki',
+    }
+}
+
