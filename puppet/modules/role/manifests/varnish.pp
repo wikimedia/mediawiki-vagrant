edcea2a2bead74498cbecc32f324b2d80000e2be
Matthew Flaschen <mflaschen@wikimedia.org>
2015-11-18 14:49:35 -0500

    Have Flow depend on Varnish
    This should help us stay aware of potentially caching-dependent
    issues, such as T116095.
    
    For consistent use of Varnish, always access 127.0.0.1:6081
    instead of 127.0.0.1:8080.  Due to T118605 , you currently
    still need to do:
    
    vagrant enable-role varnish
    
    Otherwise port forwarding will not be set up.
    
    However, this will hopefully at least make people think about it.
    
    Also, clarify varnish role docs.
    
    Bug: T116095
    Change-Id: I3dbe6800b7d7d0309c05f0fa8525c1d17ad0f0bf

--- puppet/modules/role/manifests/varnish.pp
+++ puppet/modules/role/manifests/varnish.pp
@@ -1,7 +1,7 @@
 # == Class: role::varnish
 #
 # Installs a Varnish instance
-#
+# To go through Varnish, use 127.0.0.1:6081 instead of 127.0.0.1:8080.
 class role::varnish {
     include ::varnish
 
@@ -12,4 +12,3 @@ class role::varnish {
         }
     }
 }
-
