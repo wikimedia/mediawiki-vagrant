e2124f88dadd1944138b041d3d7c9c3c47d6b843
Gilles Dubuc <gdubuc@wikimedia.org>
2015-11-19 15:04:30 +0100

    Set same image dimensions limits as production for multimedia
    Change-Id: Id6e2b13e0b50a9dd47b70f4e080a6a3c6990d047

--- puppet/modules/role/manifests/multimedia.pp
+++ puppet/modules/role/manifests/multimedia.pp
@@ -2,6 +2,7 @@
 # This class configures MediaWiki for multimedia development:
 # - installs ImageMagick
 # - raises file size limits
+# - raises image MP limit
 # - configures 404-handler[https://www.mediawiki.org/wiki/Manual:Thumb.php#404_Handler]
 # - installs exiftool
 # It is meant to contain general configuration of shared use to other
@@ -15,6 +16,14 @@ class role::multimedia {
         settings => {
             upload_max_filesize => '100M',
             post_max_size       => '100M',
+        }
+    }
+
+    mediawiki::settings { 'multimedia':
+        values => {
+            'wgMaxImageArea'       => 75e6,
+            'wgTiffMaxMetaSize'    => 1048576,
+            'wgMaxAnimatedGifArea' => 75e6,
         }
     }
 
