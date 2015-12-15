8405ad3bd6dbe03e2818d1abbd3047e5606bbfcf
Adam Roses Wight <awight@wikimedia.org>
2015-12-14 22:36:06 -0800

    Role for the ORES extension
    Configures with a test data source.
    
    Bug: T118584
    Bug: T118039
    Change-Id: I486da642e67b369732dce2d4408e47eee67ff372

---
+++ puppet/modules/role/manifests/ores.pp
@@ -0,0 +1,32 @@
+# == Class: role::ores
+#
+# Install the MediaWiki ORES extension and configure in test data mode.
+#
+# TODO:
+# - Also provision an ORES server.
+#
+class role::ores {
+    mediawiki::extension { 'ORES':
+        needs_update => true,
+        settings     => {
+            # Use the staging server until production supports 'damaging'
+            wgOresBaseUrl => 'https://ores.wmflabs.org/',
+
+            # Point at some fake data with flat probability distribution.
+            wgOresWikiId  => 'testwiki',
+
+            # This parameter is supposed to be an array, but we're working
+            # around T121378 and a limitation in the testwiki pseudomodels.
+            wgOresModels  => 'damaging',
+        },
+    }
+
+    exec { 'check ORES model versions':
+        command => '/usr/local/bin/mwscript extensions/ORES/maintenance/CheckModelVersions.php --wiki=wiki',
+        unless  => 'mysql -e "select * from ores_model" wiki | /bin/grep -q "damaging"',
+        require => [
+            Mediawiki::Extension['ORES'],
+            Exec['update_all_databases'],
+        ],
+    }
+}
