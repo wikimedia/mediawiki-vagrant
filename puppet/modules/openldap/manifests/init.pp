fea85e334a0354b7db5a296a9ecbcf8066f78619
Bryan Davis <bd808@wikimedia.org>
2016-03-15 18:33:42 -0600

    Add LdapAuthentication role
    Add a role to provision an openldap server and "ldapauth" wiki.
    
    The module that adds the LDAP server is pretty basic and tuned to work
    with ::role::ldapauth. It shouldn't be mistaken for a general purpose
    Puppet module.
    
    Bug: T128501
    Change-Id: I11eb9e8ae9dc26ba9ea52219b770d9736cb914b3

---
+++ puppet/modules/openldap/manifests/init.pp
@@ -0,0 +1,94 @@
+# == Class: openldap
+#
+# This class installs slapd and configures it with a single suffix hdb
+# database. The implementation here is tuned to work with ::role::ldapauth and
+# should not be mistaken for a reusable Puppet module.
+#
+# Based loosely on the openldap class from
+# https://phabricator.wikimedia.org/diffusion/OPUP/
+#
+# === Parameters
+#
+# [*suffix*]
+#   Distinguished name of the root of the subtree managed by this server.
+#
+# [*datadir*]
+#   The datadir this suffix will be installed, e.g. "/var/lib/ldap"
+#
+# [*admin_dn*]
+#   Distinguished name of admin user.
+#
+# [*admin_password*]
+#   Password for admin user.
+#
+# [*logging*]
+#   Specify the kind of logging desired. Defaults to "sync stats" And it is
+#   not named loglevel cause that's a puppet metaparameter
+#
+class openldap(
+    $suffix,
+    $datadir,
+    $admin_dn,
+    $admin_password,
+    $logging = 'sync stats',
+) {
+    require_package('slapd', 'ldap-utils', 'python-ldap')
+
+    # Remove the package provided ldap-based config system so that we can just
+    # hardcode the config in /etc/ldap/slapd.conf
+    exec { 'rm_slapd.d':
+        onlyif  => '/usr/bin/test -d /etc/ldap/slapd.d',
+        command => '/bin/rm -rf /etc/ldap/slapd.d',
+        require => Package['slapd'],
+    }
+
+    file { $datadir:
+        ensure  => directory,
+        recurse => false,
+        owner   => 'openldap',
+        group   => 'openldap',
+        mode    => '0750',
+        force   => true,
+        require => Package['slapd'],
+    }
+
+    file { '/etc/ldap/slapd.conf' :
+        ensure  => present,
+        owner   => 'openldap',
+        group   => 'openldap',
+        mode    => '0440',
+        content => template('openldap/slapd.erb'),
+        require => Package['slapd'],
+        notify  => Service['slapd']
+    }
+
+    file { '/etc/default/slapd' :
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('openldap/default.erb'),
+        require => Package['slapd'],
+        notify  => Service['slapd']
+    }
+
+    file { '/etc/ldap/ldap.conf':
+        ensure  => present,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0444',
+        content => template('openldap/ldap.conf.erb'),
+        require => Package['slapd'],
+    }
+
+    service { 'slapd':
+        ensure     => running,
+        hasstatus  => true,
+        hasrestart => true,
+        require    => [
+            Exec['rm_slapd.d'],
+            File[$datadir],
+            File['/etc/ldap/ldap.conf'],
+        ]
+    }
+}
---
+++ puppet/modules/role/manifests/ldapauth.pp
@@ -0,0 +1,57 @@
+# == Class: role::ldapauth
+# Provisions and LDAP server and the LdapAuthentication extension for use by
+# a wiki named ldapauth.wiki.local.wmftest.net.
+#
+# === Parameters
+# [*proxy_agent_password*]
+#   Password for proxy agent account
+#
+# [*writer_password*]
+#   Password for account with write access
+#
+# [*admin_password*]
+#   Password for LDAP admin account
+#
+class role::ldapauth(
+    $proxy_agent_password,
+    $writer_password,
+    $admin_password,
+) {
+    # Needed for php5 maintenance scripts and if the VM is using role::zend
+    require_package('php5-ldap')
+
+    # This is a lazy short cut so we don't have to pass a bazillion options to
+    # create the initial LDIF data.
+    $base_dn = 'dc=wmftest,dc=net'
+    $admin_dn = "cn=admin,${base_dn}"
+    $user_base_dn = "ou=People,${base_dn}"
+    $proxy_agent_dn = "cn=proxyagent,${base_dn}"
+    $writer_dn = "cn=writer,${base_dn}"
+
+    class { '::openldap':
+        suffix         => $base_dn,
+        datadir        => '/var/lib/ldap',
+        admin_dn       => $admin_dn,
+        admin_password => $admin_password,
+    }
+
+    exec { 'Create LDAP db':
+        command => template('role/ldapauth/create_db.erb'),
+        unless  => template('role/ldapauth/check_db.erb'),
+        require => Class['::openldap'],
+    }
+
+    mediawiki::wiki { 'ldapauth':
+        wgconf => {
+            'wmvExtensions' => {
+                  'CentralAuth' => false,
+            },
+        },
+    }
+
+    mediawiki::extension { 'LdapAuthentication':
+        needs_update => true,
+        settings     => template('role/ldapauth/LdapAuthentication.php.erb'),
+        wiki         => 'ldapauth',
+    }
+}
