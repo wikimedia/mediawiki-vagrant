02cdd69f30a734b07a2f691c4f7303615310dbcd
Gilles Dubuc <gdubuc@wikimedia.org>
2016-04-07 22:30:14 +0200

    Vagrant role for 3d extension
    Bug: T132094
    Change-Id: I89f191cfc02f40c1d7364943de9cb61f791f48ae

---
+++ puppet/modules/3d/manifests/init.pp
@@ -0,0 +1,58 @@
+# == Class: 3d
+#
+# This Puppet class installs and configures the binaries needed by the 3d extensions.
+#
+# === Parameters
+#
+# [*3d2png_dir*]
+#   Path where 3d2png should be installed (example: '/var/3d2png').
+#
+class 3d (
+    $3d2png_dir,
+) {
+    require_package('pkg-config')
+    require_package('libcairo2-dev')
+    require_package('libjpeg-dev')
+    require_package('libxi-dev')
+    require_package('libglu1-mesa-dev')
+    require_package('libglew-dev')
+
+    file { $3d2png_dir:
+        ensure => directory,
+        owner  => 'vagrant',
+    }
+
+    git::clone { '3d2png':
+        directory => $3d2png_dir,
+        remote    => 'https://phabricator.wikimedia.org/diffusion/TDTP/3d2png.git',
+        owner     => 'vagrant',
+    }
+
+    # the gl module needs prebuild installed for the --no-bin-links option
+    exec { '3d2png_prebuild':
+        command => '/usr/bin/npm install prebuild',
+        cwd     => $3d2png_dir,
+        user    => 'vagrant',
+        creates => "${3d2png_dir}/node_modules/prebuild",
+        require => [
+            Git::Clone['3d2png'],
+        ],
+    }
+
+    # cannot use npm::install because by that point node_modules already exists
+    exec { '3d2png_npm_install':
+        command     => '/usr/bin/npm install --no-bin-links',
+        cwd         => $3d2png_dir,
+        user        => 'vagrant',
+        environment => [
+            "NPM_CONFIG_CACHE=${::npm::cache_dir}",
+            'NPM_CONFIG_GLOBAL=false',
+            'LINK=g++',
+            'HOME=/home/vagrant',
+        ],
+        creates     => "${3d2png_dir}/node_modules/yargs",
+        require     => [
+            Exec['3d2png_prebuild'],
+        ],
+    }
+}
