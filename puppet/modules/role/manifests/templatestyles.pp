e09721f38b3034d44dec787fef51d0a2079fa734
Bryan Davis <bd808@wikimedia.org>
2016-04-22 10:22:58 -0600

    Add role::templatestyles
    Change-Id: I7892a1a82f48921dfabe4e0f4eeeee52c97829c7

---
+++ puppet/modules/role/manifests/templatestyles.pp
@@ -0,0 +1,12 @@
+# == Class: role::templatestyles
+# Installs the [TemplateStyles][1] extension which introduces
+# a `<templatestyles>` tag to supply a CSS stylesheet for a particular
+# template.
+#
+# [1] https://www.mediawiki.org/wiki/Extension:TemplateStyles
+class role::templatestyles {
+    require ::role::mediawiki
+
+    mediawiki::extension { 'TemplateStyles': }
+}
+
