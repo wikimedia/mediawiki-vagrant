1c4e14a93a03cb3a21de546e587645f6ec02a35c
Yuri Astrakhan <yurik@wikimedia.org>
2016-05-18 22:39:59 +0300

    Updated graphoid role
    TODO: Configure restbase to pass-through the graphoid call
          If restbase is not present, configuration should be changed
    
    Bug: T135549
    Change-Id: Iede0db7d63e92435742c03550e9d7066439e3d0a

--- puppet/modules/graphoid/manifests/init.pp
+++ puppet/modules/graphoid/manifests/init.pp
@@ -4,59 +4,53 @@
 #
 # === Parameters
 #
-# [*base_path*]
-#   Path to the graphoid code. (e.g. /vagrant/graphoid)
-#
-# [*node_path*]
-#   Path to the node modules graphoid depends on.
-#   (e.g. /vagrant/graphoid/node_modules)
-#
-# [*conf_path*]
-#   Where to place the config file.
-#   (e.g. /vagrant/graphoid/graphoid.config.json)
-#
-# [*log_dir*]
-#   Place where graphoid can put log files. Assumed to be already existing and
-#   have write access to graphoid user. (e.g. /vagrant/logs)
-#
 # [*port*]
-#   Port the graphoid service listens on for incoming connections. (e.g 11042)
+#   Port the service listens on for incoming connections.
+#
+# [*log_level*]
+#   The lowest level to log (trace, debug, info, warn, error, fatal)
+#
+# [*allowed_domains*]
+#   The protocol-to-list-of-domains map. Default: {}
+#   The protocols include http, https, as well as some custom graph-specific protocols.
+#   See https://www.mediawiki.org/wiki/Extension:Graph?venotify=restored#External_data
+#
+# [*domain_map*]
+#   The domain-to-domain alias map. Default: {}
+#
+# [*timeout*]
+#   The timeout (in ms) for requests. Default: 5000
+#
+# [*headers*]
+#   A map of headers that will be sent with each reply. Could be used for caching, etc. Default: false
+#
+# [*error_headers*]
+#   A map of headers that will be sent with each reply in case of an error. If not set, above headers will be used. Default: false
 #
 class graphoid(
-    $base_path,
-    $node_path,
-    $conf_path,
-    $log_dir,
     $port,
+    $log_level = undef,
+    $allowed_domains = {},
+    $domain_map    = {},
+    $timeout       = 5000,
+    $headers       = false,
+    $error_headers = false,
 ) {
 
-    $log_file = "${log_dir}/graphoid.log"
-
-    require_package('nodejs')
     require_package('libcairo2-dev')
     require_package('libjpeg-dev')
     require_package('libgif-dev')
 
-    file { $conf_path:
-        ensure  => present,
-        content => template('graphoid/config.erb'),
+    service::node { 'graphoid':
+        port      => $port,
+        log_level => $log_level,
+        config    => {
+            allowedDomains => $allowed_domains,
+            domainMap      => $domain_map,
+            timeout        => $timeout,
+            headers        => $headers,
+            errorHeaders   => $error_headers,
+        },
     }
 
-    # The upstart configuration
-    file { '/etc/init/graphoid.conf':
-        ensure  => present,
-        owner   => root,
-        group   => root,
-        mode    => '0444',
-        content => template('graphoid/upstart.erb'),
-    }
-
-    service { 'graphoid':
-        ensure     => running,
-        enable     => true,
-        hasstatus  => true,
-        hasrestart => true,
-        provider   => 'upstart',
-        subscribe  => File['/etc/init/graphoid.conf'],
-    }
 }
--- puppet/modules/role/manifests/graphoid.pp
+++ puppet/modules/role/manifests/graphoid.pp
@@ -2,11 +2,16 @@
 # This role installs the graphoid service for server side graph rendering.
 #
 class role::graphoid {
-    require ::graphoid::install::git
+    include ::graphoid
+
     # use local graphoid renderer
     mediawiki::settings { 'Graphoid':
         values => [
-            '$wgGraphImgServiceUrl = "//$wgServerName:11042?server=%1\$s&title=%2\$s&revid=%3\$s&id=%4\$s";',
+            # Using list declaration because of some weird escaping rules
+            '$wgGraphImgServiceUrl = "/api/rest_v1/page/graph/png/%2\$s/%3\$s/%4\$s.png";',
+
+            # TODO: if restbase is not set up, use this pattern (host, port, and server should be dynamic?)
+            '$wgGraphImgServiceUrl = "//localhost:11042?server=%1\$s&title=%2\$s&revid=%3\$s&id=%4\$s";',
         ],
     }
 }
