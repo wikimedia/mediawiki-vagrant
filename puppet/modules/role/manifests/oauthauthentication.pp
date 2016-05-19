dd4330a1c02ff0e4ae75553aff6baca482783339
Gergő Tisza <tgr.huwiki@gmail.com>
2016-05-20 01:47:18 +0200

    Move role::oauthauthentication OAuth config to hiera
    Change-Id: Iace02f3d81700ca962a05922ac48b7bae6a97ac3

--- puppet/modules/role/manifests/oauthauthentication.pp
+++ puppet/modules/role/manifests/oauthauthentication.pp
@@ -4,17 +4,21 @@
 #
 # [1] https://www.mediawiki.org/wiki/Extension:OAuthAuthentication
 #
-class role::oauthauthentication {
+class role::oauthauthentication (
+    $consumer_key,
+    $consumer_secret,
+    $callback_url,
+) {
     mediawiki::extension { 'OAuthAuthentication':
         needs_update => true,
         composer     => true,
         settings     => {
             wgOAuthAuthenticationUrl            => 'https://meta.wikimedia.org/w/index.php?title=Special:OAuth',
-            wgOAuthAuthenticationConsumerKey    => '0fde3e1e451907e9653ea612f7b30a5a',
-            wgOAuthAuthenticationConsumerSecret => '143a7e53fe1e0fe7f42a2aed77b8e6fc9f38112a',
+            wgOAuthAuthenticationConsumerKey    => $consumer_key,
+            wgOAuthAuthenticationConsumerSecret => $consumer_secret,
             wgOAuthAuthenticationCanonicalUrl   => 'https://meta.wikimedia.org',
             wgOAuthAuthenticationRemoteName     => 'Wikimedia',
-            wgOAuthAuthenticationCallbackUrl    => "http://dev.wiki.local.wmftest.net${::port_fragment}/wiki/Special:OAuthLogin/finish",
+            wgOAuthAuthenticationCallbackUrl    => $callback_url,
         }
     }
 
