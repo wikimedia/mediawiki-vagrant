de41e0670db6179ff6b190e52d262d3823e485c4
Yuri Astrakhan <yurik@wikimedia.org>
2016-10-08 11:26:48 -0400

    Kartographer depends on wikimediamessages
    Change-Id: I0064650d32e8332aa7a9f7cebc54f388cc192ec1

--- puppet/modules/role/manifests/kartographer.pp
+++ puppet/modules/role/manifests/kartographer.pp
@@ -1,6 +1,9 @@
 # == Class: role::kartographer
 # Configures Kartographer, an extension for display maps in wiki pages
 class role::kartographer {
+
+    include ::role::wikimediamessages
+
     mediawiki::extension { 'Kartographer':
         composer => true,
     }
