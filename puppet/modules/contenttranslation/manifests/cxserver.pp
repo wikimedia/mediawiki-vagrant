f5c3919154a2130f81f41dd8b313781b3cb99ad9
Kartik Mistry <kartik.mistry@gmail.com>
2016-11-08 16:40:58 +0530

    Update Contenttranslation role
    * Properly migrate to service-runner.
    * Removed obsolete experimental flag.
    
    Bug: T149880
    Change-Id: Ie284f3e3abec0b9727f70ed5ea8182360edcbcd9

--- puppet/modules/contenttranslation/manifests/cxserver.pp
+++ puppet/modules/contenttranslation/manifests/cxserver.pp
@@ -7,60 +7,33 @@
 #
 # == Parameters
 #
-# [*dir*]
-#   The directory in which to install cxserver.
-#   Defaults to /srv/cxserver.
-#
 # [*port*]
 #   The port the cxserver should listen on.
 #   Defaults to 8090.
 #
-# [*proxy*]
-#   Forward proxy to use when connecting to machine translation services.
-#   Defaults to 'null' for no proxy.
-#
-# [*log*]
-#   Log directory.
-#   Defaults to 'log'.
-#
-# [*allow_cors*]
-#   Hosts for CORS. Defaults to '*'.
+# [*dir*]
+#   The directory in which to install cxserver.
+#   Defaults to /srv/cxserver.
 #
 # [*apertium*]
 #   The url for the apertium machine translation service.
 #   Defaults to '//apertium.wmflabs.org'
 #
-# [*yandex*]
+# [*yandex_url*]
 #   The url for the yandex machine translation service.
 #   Defaults to 'https://translate.yandex.net'
 #
-# [*yandex_key*]
+# [*yandex_api_key*]
 #   Api key for yandex translation service.
 #   Defaults to 'null'.
 #
-# [*youdao*]
+# [*youdao_url*]
 #   The url for the youdao machine translation service.
 #   Defaults to 'https://fanyi.youdao.com/paidapi/fanyiapi'
 #
-# [*youdao_key*]
+# [*youdao_api_key*]
 #   Api key for youdao translation service.
 #   Defaults to 'null'.
-#
-# [*secure*]
-#   Whether or not to run cxserver on https.
-#   Defaults to 'false'.
-#
-# [*ssl_key*]
-#   Path to ssl key file.
-#   Defaults to 'null'.
-#
-# [*cert*]
-#   Path to cert file.
-#   Defaults to 'null'.
-#
-# [*workers*]
-#   Number of workers to spawn when starting cxserver.
-#   Defaults to 2.
 #
 # == Example
 #
@@ -75,44 +48,19 @@
 #   and include entries for the settings you want to override.
 #
 class contenttranslation::cxserver(
-    $dir,
     $port,
-    $proxy,
-    $log,
-    $allow_cors,
+    $dir,
     $apertium,
-    $yandex,
-    $yandex_key,
-    $youdao,
-    $youdao_key,
-    $secure,
-    $ssl_key,
-    $cert,
-    $workers,
+    $yandex_url,
+    $yandex_api_key,
+    $youdao_url,
+    $youdao_api_key,
+    $registry = 'registry.yaml',
 ) {
-    require ::npm
+    require ::service
 
-    git::clone { 'mediawiki/services/cxserver/deploy':
-        directory => $dir,
-        owner     => $::share_owner,
-        group     => $::share_group,
-    }
-
-    file { "${dir}/src/config.yaml":
-        content => template('contenttranslation/cxserver.config.yaml.erb'),
-        require => Git::Clone['mediawiki/services/cxserver/deploy'],
-    }
-
-    file { '/etc/init/cxserver.conf':
-        content => template('contenttranslation/cxserver.conf.erb'),
-    }
-
-    service { 'cxserver':
-        ensure    => running,
-        provider  => 'upstart',
-        subscribe => [
-            File['/etc/init/cxserver.conf'],
-            File["${dir}/src/config.yaml"],
-        ],
+    service::node { 'cxserver':
+        port      => $port,
+        config    => template('contenttranslation/cxserver.config.yaml.erb'),
     }
 }
