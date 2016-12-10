97bc5ea1cf9a12b08a314292ff1782374f1152df
Gergő Tisza <tgr.huwiki@gmail.com>
2016-12-09 17:18:23 -0800

    Update Reading Web roles
    * add RelatedArticles
    * add WikidataPageBanner
    * add QuickSurveys
    * remove Gather
    
    The roles uses the minimal required dependencies (relatedarticles
    does not install BetaFeatures; wikidatapagebanner does not install
    Wikibase or PageImages); those can be installed as separate roles
    if needed.
    
    Bug: T152036
    Change-Id: I4f8567454b48c1a7ccddc9f1c72df3bc746159dd

---
+++ puppet/modules/role/manifests/relatedarticles.pp
@@ -0,0 +1,20 @@
+# == Class: role::relatedarticles
+#
+# Installs the RelatedArticles[1] extension which shows some related
+# articles (based on content similarity, can be overriden via a parser
+# tag).
+#
+# [1] https://www.mediawiki.org/wiki/Extension:RelatedArticles
+#
+class role::relatedarticles {
+    mediawiki::extension { 'RelatedArticles':
+        settings => {
+            wgRelatedArticlesShowInSidebar   => false,
+            wgRelatedArticlesShowInFooter    => true,
+            wgRelatedArticlesUseCirrusSearch => true,
+        },
+        require  => MediaWiki::Extension['Cards'],
+    }
+
+    mediawiki::extension { 'Cards': }
+}
