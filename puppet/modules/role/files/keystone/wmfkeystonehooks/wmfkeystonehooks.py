c11725960472bebd601459831d065d2deef3bf22
Bryan Davis <bd808@wikimedia.org>
2017-04-09 20:30:03 -0600

    striker: support for project membership management
    * Update wmfkeystonehooks to latest upstream version
    * Add configuration for keystone client
    
    Change-Id: Ie37c3431bc859afe67f6356b8fb01788a54b0bc0

--- puppet/modules/role/files/keystone/wmfkeystonehooks/wmfkeystonehooks.py
+++ puppet/modules/role/files/keystone/wmfkeystonehooks/wmfkeystonehooks.py
@@ -12,6 +12,7 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+import ldapgroups
 
 from keystoneclient.auth.identity import generic
 from keystoneclient import session as keystone_session
@@ -25,7 +26,7 @@ from oslo_config import cfg
 from oslo_messaging.notify import notifier
 
 # These imports are for the id monkeypatch at the bottom of this
-# file.  Including up here to make flake8 happy
+#  file.  Including up here to make flake8 happy
 from keystone.resource import controllers as resource_controllers
 from keystone.common import controller
 from keystone.common import validation
@@ -58,6 +59,18 @@ wmfkeystone_opts = [
     cfg.StrOpt('admin_role_name',
                default='projectadmin',
                help='Name of project-local admin role'),
+    cfg.StrOpt('ldap_group_base_dn',
+               default='ou=groups,dc=wikimedia,dc=org',
+               help='ldap dn for posix groups'),
+    cfg.StrOpt('ldap_user_base_dn',
+               default='ou=people,dc=wikimedia,dc=org',
+               help='ldap dn for user accounts'),
+    cfg.StrOpt('ldap_project_base_dn',
+               default='ou=projects,dc=wikimedia,dc=org',
+               help='ldap dn for project records'),
+    cfg.IntOpt('minimum_gid_number',
+               default=40000,
+               help='Starting gid number for posix groups'),
     cfg.MultiStrOpt('eventtype_whitelist',
                     default=['identity.project.deleted', 'identity.project.created'],
                     help='Event types to always handle.'),
@@ -80,10 +93,159 @@ class KeystoneHooks(notifier.Driver):
     def __init__(self, conf, topics, transport, version=1.0):
         pass
 
-    # The upstream implementation here has been removed. We just want this
-    # stub so that our file is loaded and the monkey patch below takes effect.
-    # -- bd808 2017-03-21
+    def _get_role_dict(self):
+        rolelist = self.role_api.list_roles()
+        roledict = {}
+        # Make a dict to relate role names to ids
+        for role in rolelist:
+            roledict[role['name']] = role['id']
+
+        return roledict
+
+    def _get_current_assignments(self, project_id):
+        reverseroledict = dict((v, k) for k, v in self._get_role_dict().iteritems())
+
+        rawassignments = self.assignment_api.list_role_assignments(project_id=project_id)
+        assignments = {}
+        for assignment in rawassignments:
+            rolename = reverseroledict[assignment["role_id"]]
+            if rolename not in assignments:
+                assignments[rolename] = set()
+            assignments[rolename].add(assignment["user_id"])
+        return assignments
+
+    # There are a bunch of different events which might update project membership,
+    #  and the generic 'identity.projectupdated' comes in the wrong order.  So
+    #  we're probably going to wind up getting called several times in quick succession,
+    #  possible in overlapping invocations.  Watch out for race conditions!
+    def _on_member_update(self, project_id):
+        assignments = self._get_current_assignments(project_id)
+        ldapgroups.sync_ldap_project_group(project_id, assignments)
+
+    def _on_project_delete(self, project_id):
+        ldapgroups.delete_ldap_project_group(project_id)
+
+    def _on_project_create(self, project_id):
+
+        LOG.warning("Beginning wmf hooks for project creation: %s" % project_id)
+
+        roledict = self._get_role_dict()
+
+        if CONF.wmfhooks.observer_role_name not in roledict.keys():
+            LOG.error("Failed to find id for role %s" % CONF.wmfhooks.observer_role_name)
+            raise exception.NotImplemented()
+        if CONF.wmfhooks.admin_role_name not in roledict.keys():
+            LOG.error("Failed to find id for role %s" % CONF.wmfhooks.admin_role_name)
+            raise exception.NotImplemented()
+        if CONF.wmfhooks.user_role_name not in roledict.keys():
+            LOG.error("Failed to find id for role %s" % CONF.wmfhooks.user_role_name)
+            raise exception.NotImplemented()
+
+        self.assignment_api.add_role_to_user_and_project(CONF.wmfhooks.admin_user,
+                                                         project_id,
+                                                         roledict[CONF.wmfhooks.admin_role_name])
+        self.assignment_api.add_role_to_user_and_project(CONF.wmfhooks.admin_user,
+                                                         project_id,
+                                                         roledict[CONF.wmfhooks.user_role_name])
+        self.assignment_api.add_role_to_user_and_project(CONF.wmfhooks.observer_user,
+                                                         project_id,
+                                                         roledict[CONF.wmfhooks.observer_role_name])
+
+        # Use the nova api to set up security groups for the new project
+        auth = generic.Password(
+            auth_url=CONF.wmfhooks.auth_url,
+            username=CONF.wmfhooks.admin_user,
+            password=CONF.wmfhooks.admin_pass,
+            user_domain_name='Default',
+            project_domain_name='Default',
+            project_name=project_id)
+        session = keystone_session.Session(auth=auth)
+        client = nova_client.Client('2', session=session, connect_retries=5)
+        allgroups = client.security_groups.list()
+        defaultgroup = filter(lambda group: group.name == 'default', allgroups)
+        if defaultgroup:
+            groupid = defaultgroup[0].id
+            try:
+                client.security_group_rules.create(groupid,
+                                                   ip_protocol='icmp',
+                                                   from_port='-1',
+                                                   to_port='-1',
+                                                   cidr='0.0.0.0/0')
+            except (exceptions.ClientException):
+                LOG.warning("icmp security rule already exists.")
+            try:
+                client.security_group_rules.create(groupid,
+                                                   ip_protocol='tcp',
+                                                   from_port='22',
+                                                   to_port='22',
+                                                   cidr='10.0.0.0/8')
+            except (exceptions.ClientException):
+                LOG.warning("Port 22 security rule already exists.")
+            try:
+                client.security_group_rules.create(groupid,
+                                                   ip_protocol='tcp',
+                                                   from_port='5666',
+                                                   to_port='5666',
+                                                   cidr='10.0.0.0/8')
+            except (exceptions.ClientException):
+                LOG.warning("Port 5666 security rule already exists.")
+            try:
+                client.security_group_rules.create(groupid,
+                                                   ip_protocol='tcp',
+                                                   from_port='1',
+                                                   to_port='65535',
+                                                   cidr='',
+                                                   group_id=groupid)
+            except (exceptions.ClientException):
+                LOG.warning("Project security rule for TCP already exists.")
+
+            try:
+                client.security_group_rules.create(groupid,
+                                                   ip_protocol='udp',
+                                                   from_port='1',
+                                                   to_port='65535',
+                                                   cidr='',
+                                                   group_id=groupid)
+            except (exceptions.ClientException):
+                LOG.warning("Project security rule for UDP already exists.")
+
+            try:
+                client.security_group_rules.create(groupid,
+                                                   ip_protocol='icmp',
+                                                   from_port='1',
+                                                   to_port='65535',
+                                                   cidr='',
+                                                   group_id=groupid)
+            except (exceptions.ClientException):
+                LOG.warning("Project security rule for ICMP already exists.")
+        else:
+            LOG.warning("Failed to find default security group in new project.")
+
+        assignments = self._get_current_assignments(project_id)
+        ldapgroups.sync_ldap_project_group(project_id, assignments)
+
+        # Set up default sudoers in ldap
+        ldapgroups.create_sudo_defaults(project_id)
+
     def notify(self, context, message, priority, retry=False):
+        event_type = message.get('event_type')
+
+        if event_type == 'identity.project.deleted':
+            self._on_project_delete(message['payload']['resource_info'])
+
+        if event_type == 'identity.project.created':
+            self._on_project_create(message['payload']['resource_info'])
+
+        if (event_type == 'identity.role_assignment.deleted' or
+                event_type == 'identity.role_assignment.created'):
+            self._on_member_update(message['payload']['project'])
+
+        # Eventually this will be used to update project resource pages:
+        if event_type in CONF.wmfhooks.eventtype_blacklist:
+            return
+        if event_type not in CONF.wmfhooks.eventtype_whitelist:
+            return
+
         return
 
 
