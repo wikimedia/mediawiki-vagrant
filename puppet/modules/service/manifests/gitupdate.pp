93288ccf55eb03cadf9bfd346669d4010250b095
Petr Pchelko <ppchelko@wikimedia.org>
2017-07-13 11:15:28 -0700

    Puppet: Only install production packages for node.
    Node services git-update takes ages to complete.
    Speed it up a little bit by only installing production
    dependencies and not installing devDependencies.
    
    Change-Id: Ia517810de8c562494ffee7bdf586f7768cb21b86

--- puppet/modules/service/manifests/gitupdate.pp
+++ puppet/modules/service/manifests/gitupdate.pp
@@ -86,7 +86,7 @@ define service::gitupdate(
     # descern the update command to use
     $up_cmd = $type ? {
         'php'    => 'composer update --no-interaction --optimize-autoloader',
-        'nodejs' => 'sudo rm -rf node_modules && npm install --no-bin-links',
+        'nodejs' => 'sudo rm -rf node_modules && npm install --no-bin-links --production',
         'python' => './virtualenv/bin/pip install -Ue .',
         default  => 'invalid'
     }
