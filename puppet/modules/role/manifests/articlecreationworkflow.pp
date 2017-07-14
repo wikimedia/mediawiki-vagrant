3a0936cda64a7a54514c38bfcee27d17e7a8dd2e
Max Semenik <maxsem.wiki@gmail.com>
2017-07-14 11:59:07 -0700

    Add a role for ArticleCreationWorkflow
    Bug: T170354
    Change-Id: I842e363b60d419cdf27711fd2253384db2d911bd

---
+++ puppet/modules/role/manifests/articlecreationworkflow.pp
@@ -0,0 +1,7 @@
+# == Class: role::articlecreationworkflow
+# The ArticleCreationWorkflow[1] allows to customize page creation experience for new users.
+#
+# [1] https://www.mediawiki.org/wiki/Extension:ArticleCreationWorkflow
+class role::articlecreationworkflow {
+  mediawiki::extension { 'ArticleCreationWorkflow': }
+}
