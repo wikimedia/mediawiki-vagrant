b7a10052332a6da8b64e11cb38928be0119c4173
mainframe98 <k.s.werf@hotmail.com>
2017-07-28 13:54:43 +0200

    Require MobileFrontend to be installed when enabling Minerva
    Enabling the Minerva role fails because it depends on MobileFrontend
    and will throw an exception during the provisioning process.
    
    Bug: T171942
    Change-Id: I3f97ea6e5eb6bab4ce6f5e740034ca03e4bbdfb6

--- puppet/modules/role/manifests/minerva.pp
+++ puppet/modules/role/manifests/minerva.pp
@@ -2,5 +2,8 @@
 # Configures Minerva, a MediaWiki skin, as an option.
 # https://www.mediawiki.org/wiki/Skin:MinervaNeue
 class role::minerva {
+    require ::role::mediawiki
+    include ::role::mobilefrontend
+
     mediawiki::skin { 'MinervaNeue': }
 }
