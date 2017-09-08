231f168a9a3cc6dc0486baab105c1814ae00181b
Max Semenik <maxsem.wiki@gmail.com>
2017-09-08 16:44:49 -0700

    Make sure ACW gets loaded before VE
    Change-Id: Ie5045dd89ea2f3fb9d49c5871896f6fc0820617c

--- puppet/modules/role/manifests/articlecreationworkflow.pp
+++ puppet/modules/role/manifests/articlecreationworkflow.pp
@@ -3,5 +3,7 @@
 #
 # [1] https://www.mediawiki.org/wiki/Extension:ArticleCreationWorkflow
 class role::articlecreationworkflow {
-  mediawiki::extension { 'ArticleCreationWorkflow': }
+  mediawiki::extension { 'ArticleCreationWorkflow':
+      priority => $::LOAD_EARLY, # Must load before VisualEditor
+  }
 }
