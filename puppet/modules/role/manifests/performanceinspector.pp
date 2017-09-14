41e2bc53c3b77c15f05f927eab4df0b868af6622
Gilles Dubuc <gilles@wikimedia.org>
2017-09-14 12:47:17 +0200

    Add betafeatures dependency for performanceinspector role
    Bug: T146045
    Change-Id: Iaecb9dd95e55678e764c394fc479d0aeb59149a5

--- puppet/modules/role/manifests/performanceinspector.pp
+++ puppet/modules/role/manifests/performanceinspector.pp
@@ -2,6 +2,8 @@
 # Configures PerformanceInspector, a MediaWiki extension that shows
 # performance metrics on the client side.
 class role::performanceinspector {
+    include ::role::betafeatures
+
     mediawiki::extension { 'PerformanceInspector':
     }
 }
