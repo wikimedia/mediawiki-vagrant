4659638e2ea1089d22712fb10db2aa1c93ededdc
Gilles Dubuc <gilles@wikimedia.org>
2017-12-20 15:17:13 +0100

    Fix Swift role for Stretch
    The new environment doesn’t like unqualified resource
    names and doesn’t propagate variable context all the
    way to the system service template. Since we don’t
    really need that, I have worked around it.
    
    Bug: T183354
    Change-Id: I6b6dc1eab2f2da4eca241020ccf0ccd752a278fd

--- puppet/modules/swift/manifests/ring.pp
+++ puppet/modules/swift/manifests/ring.pp
@@ -4,12 +4,6 @@
 #
 # === Parameters
 #
-# [*ring_type*]
-#   The type of swift ring .
-#
-# [*cfg_file*]
-#   Path to the ring's config file.
-#
 # [*storage_dir*]
 #   Path to the swift storage directory.
 #
@@ -19,46 +13,42 @@
 # === Examples
 #
 #   swift::ring { 'account':
-#       server_type => 'account',
-#       cfg_file    => '/etc/swift/account-server.conf',
 #       storage_dir => '/srv/swift',
 #       ring_port   => 6010,
 #   }
 #
 define swift::ring(
-    $ring_type,
-    $cfg_file,
     $storage_dir,
     $ring_port
 ) {
-    file { $cfg_file:
+    file { "/etc/swift/${title}.conf":
         ensure  => present,
         group   => 'www-data',
         content => template('swift/ring.conf.erb'),
         mode    => '0644',
-        notify  => Exec["${ring_type}/create_ring"],
+        notify  => Exec["${title}/create_ring"],
     }
 
-    exec { "${ring_type}/create_ring":
-        command     => "swift-ring-builder ${ring_type}.builder create 18 1 1",
+    exec { "${title}/create_ring":
+        command     => "swift-ring-builder ${title}.builder create 18 1 1",
         user        => 'swift',
         cwd         => '/etc/swift',
-        notify      => Exec["${ring_type}/add_ring"],
+        notify      => Exec["${title}/add_ring"],
         refreshonly => true,
         require     => Package['swift'],
     }
 
-    exec { "${ring_type}/add_ring":
-        command     => "swift-ring-builder ${ring_type}.builder add z1-127.0.0.1:${ring_port}/1 1",
+    exec { "${title}/add_ring":
+        command     => "swift-ring-builder ${title}.builder add z1-127.0.0.1:${ring_port}/1 1",
         user        => 'swift',
         cwd         => '/etc/swift',
-        notify      => Exec["${ring_type}/rebalance"],
+        notify      => Exec["${title}/rebalance"],
         refreshonly => true,
         require     => Package['swift'],
     }
 
-    exec { "${ring_type}/rebalance":
-        command     => "swift-ring-builder ${ring_type}.builder rebalance",
+    exec { "${title}/rebalance":
+        command     => "swift-ring-builder ${title}.builder rebalance",
         user        => 'swift',
         cwd         => '/etc/swift',
         refreshonly => true,
