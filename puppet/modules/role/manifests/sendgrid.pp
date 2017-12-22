e9ef92d1a3c93e8bdcb83a6a1c2a8cc2dcd19fe6
Alangi Derick <alangiderick@gmail.com>
2017-12-22 17:56:24 +0100

    Add role sendgrid.pp to MW-Vagrant
    Make available SendGrid extension on MW-Vagrant.
    
    Bug: T183571
    Change-Id: I59faae9105d7bd1cfe3aa3067dca39f7d2fdf43c

---
+++ puppet/modules/role/manifests/sendgrid.pp
@@ -0,0 +1,8 @@
+# == Class: role::sendgrid
+#
+# This role provisions the SendGrid extension, which allows users to
+# use the SendGrid API to send emails on-wiki using a valid API key
+#
+class role::sendgrid {
+    mediawiki::extension { 'SendGrid': }
+}
