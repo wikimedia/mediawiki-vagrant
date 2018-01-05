80f7e4c92ebc992dd221c233dfe90f600b2bb694
Gergő Tisza <tgr.huwiki@gmail.com>
2018-01-05 15:51:09 -0800

    Make swift role more developer-friendly
    Change-Id: If3dc9d2532ec154772ce9c96282cfc7ed14ce73b

--- puppet/modules/role/manifests/swift.pp
+++ puppet/modules/role/manifests/swift.pp
@@ -14,4 +14,19 @@ class role::swift {
         site    => $::mediawiki::wiki_name,
         content => template('role/swift/apache2.conf.erb'),
     }
+
+    # configuration for debugging with the 'swift' command
+    env::var { 'ST_AUTH':
+        value => "http://127.0.0.1:${::swift::port}/auth/v1.0",
+    }
+    env::var { 'ST_USER':
+        value => "${::swift::project}:${::swift::user}",
+    }
+    env::var { 'ST_KEY':
+        value => $::swift::key,
+    }
+
+    mediawiki::import::text { 'VagrantRoleSwift':
+        content => template('role/swift/VagrantRoleSwift.wiki.erb'),
+    }
 }
