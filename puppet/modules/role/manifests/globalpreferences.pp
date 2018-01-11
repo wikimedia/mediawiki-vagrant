7bad9c316da5d6465c99c85b46607175f0d0cd58
Max Semenik <maxsem.wiki@gmail.com>
2018-01-11 14:23:15 +0300

    Update role::globalpreferences
    File in question has moved
    
    Change-Id: I21f01187d9c4b2335374c0f2cb49bec2a5216910

--- puppet/modules/role/manifests/globalpreferences.pp
+++ puppet/modules/role/manifests/globalpreferences.pp
@@ -13,7 +13,7 @@ class role::globalpreferences {
     }
 
     mysql::sql { 'Create global_preferences table':
-        sql     => "USE ${shared_db}; SOURCE ${::mediawiki::dir}/extensions/GlobalPreferences/schema.sql;",
+        sql     => "USE ${shared_db}; SOURCE ${::mediawiki::dir}/extensions/GlobalPreferences/sql/tables.sql;",
         unless  => "SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE table_schema = '${shared_db}' AND table_name = 'global_preferences';",
         require => [
             Mysql::Db[$shared_db],
