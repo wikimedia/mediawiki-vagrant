03e33cce29edd43fa40b698d00ce7a45c2eb3ed6
Michael Holloway <mholloway@wikimedia.org>
2018-02-14 19:22:41 -0500

    Split off OCG into a standalone role
    OCG is no longer WMF-maintained, and people applying the offline role
    going forward will be unlikely to need or want it.
    
    This puts it in its own role so anyone still interested in hacking on it
    can do so.
    
    Bug: T187080
    Change-Id: Ic20b4e286dc68a5b8d6a3732f4ac7fa378538690

---
+++ puppet/modules/role/manifests/ocg.pp
@@ -0,0 +1,5 @@
+# == Class: role::ocg
+# Installs OCG[https://www.mediawiki.org/wiki/Offline_content_generator].
+class role::ocg {
+    include ::ocg
+}
--- puppet/modules/role/manifests/offline.pp
+++ puppet/modules/role/manifests/offline.pp
@@ -1,12 +1,10 @@
 # == Class: role::offline
 # Installs offline-related extensions/services:
 # * Collection[https://www.mediawiki.org/wiki/Extension:Collection]
-# * OCG[https://www.mediawiki.org/wiki/Offline_content_generator]
 # * ElectronPdfService[https://www.mediawiki.org/wiki/Extension:ElectronPdfService]
 # * electron[https://github.com/wikimedia/mediawiki-services-electron-render/blob/master/README.md]
 #
 class role::offline {
-    require ::ocg
     require ::electron
 
     mediawiki::settings { 'Electron-VRS':
@@ -17,6 +15,7 @@ class role::offline {
     mediawiki::extension { 'Collection':
         settings => template('role/offline/Collection.php.erb'),
     }
+
     mediawiki::extension { 'ElectronPdfService':
         settings => {
             wgElectronPdfServiceRESTbaseURL => '/api/rest_v1/page/pdf/',
