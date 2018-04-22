ca6754ccd6050e44feff001a337bde56cc3ad46b
Bryan Davis <bd808@wikimedia.org>
2018-04-22 11:39:35 +0200

    Ensure factor directories exist
    Bug: T192728
    Change-Id: I8f8d457a201df5cdb6dc72501c9fdcdce4684a20

--- support/puppet-bootstrap.sh
+++ support/puppet-bootstrap.sh
@@ -8,13 +8,13 @@ if [ "`id -u`" != "0" ]; then
     exit 1
 fi
 
-if which puppet >/dev/null 2>&1; then
-    exit 0
+if ! which puppet >/dev/null 2>&1; then
+    apt-get update >/dev/null 2>&1
+    DEBIAN_FRONTEND=noninteractive apt-get \
+        -y \
+        -o Dpkg::Options::="--force-confdef" \
+        -o Dpkg::Options::="--force-confold" \
+        install puppet >/dev/null 2>&1
 fi
-
-apt-get update >/dev/null 2>&1
-DEBIAN_FRONTEND=noninteractive apt-get \
-    -y \
-    -o Dpkg::Options::="--force-confdef" \
-    -o Dpkg::Options::="--force-confold" \
-    install puppet >/dev/null 2>&1
+# T192728: ensure some directories exist
+mkdir -p /etc/puppetlabs/facter/facts.d >/dev/null 2>&1
