9606215766e792e2889f66f027fd1ae7c84bbdfa
David Causse <dcausse@wikimedia.org>
2018-05-03 15:17:54 +0200

    wgSiteMatrixSites must have a name
    SiteMatrix.php:95 expects a 'name' entry in this config array.
    
    Change-Id: I1cd82a864180905ad8e4419dfb6fbc97bbed2a57

--- puppet/modules/role/manifests/sitematrix.pp
+++ puppet/modules/role/manifests/sitematrix.pp
@@ -3,6 +3,6 @@
 class role::sitematrix {
     mediawiki::extension { 'SiteMatrix':
         # TODO: DB postfixes other than 'wiki' aren't supported elsewhere
-        settings => "\$wgSiteMatrixSites['wiki']['host'] = 'www${mediawiki::multiwiki::base_domain}${::port_fragment}';",
+        settings => "\$wgSiteMatrixSites['wiki'] = [ 'host' => 'www${mediawiki::multiwiki::base_domain}${::port_fragment}', 'name' => 'wiki' ];",
     }
 }
