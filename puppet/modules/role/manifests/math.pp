3068011af5c5609a3f19b62fe58fccdbdfeea310
Reedy <reedy@wikimedia.org>
2018-06-07 00:46:23 +0000

    Remove math cruft
    Bug: T196590
    Change-Id: I546fc21e64a7414394b9ef5ac1e4ac5d2b81d4a3

--- puppet/modules/role/manifests/math.pp
+++ puppet/modules/role/manifests/math.pp
@@ -1,50 +1,12 @@
 # == Class: role::math
 #
 # The Math extension provides support for rendering mathematical formulas
-# on-wiki via texvc.
+# on-wiki.
 class role::math {
     require ::role::mediawiki
-
-    require_package('ghostscript')
-    require_package('imagemagick')
 
     mediawiki::extension { 'Math':
         needs_update  => true,
         browser_tests => true,
-    }
-
-    require_package(
-            'ocaml-native-compilers',
-            'texlive',
-            'texlive-bibtex-extra',
-            'texlive-font-utils',
-            'texlive-fonts-extra',
-            'texlive-lang-all',
-            'texlive-latex-extra',
-            'texlive-math-extra',
-            'texlive-pictures',
-            'texlive-pstricks',
-            'texlive-publishers',
-            'texlive-generic-extra'
-    )
-
-    exec { 'compile_texvc':
-        command => 'make',
-        cwd     => "${::mediawiki::dir}/extensions/Math/math",
-        creates => "${::mediawiki::dir}/extensions/Math/math/texvc",
-        require => [
-            Mediawiki::Extension['Math'],
-            Package['ocaml-native-compilers'],
-        ],
-    }
-
-    exec { 'compile_texvccheck':
-        command => 'make',
-        cwd     => "${::mediawiki::dir}/extensions/Math/texvccheck",
-        creates => "${::mediawiki::dir}/extensions/Math/texvccheck/texvccheck",
-        require => [
-            Mediawiki::Extension['Math'],
-            Package['ocaml-native-compilers'],
-        ],
     }
 }
