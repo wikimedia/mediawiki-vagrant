b21e69d4d815fca765e6b6c3d785a6aeb7661d5c
Stephane Bisson <sbisson@wikimedia.org>
2018-07-20 14:50:34 -0400

    Make PageTriage depend on ORES
    Bug: T200043
    Change-Id: I69218809b323a2c4d66b6e3bed39cdbb298bdc10

--- puppet/modules/role/manifests/pagetriage.pp
+++ puppet/modules/role/manifests/pagetriage.pp
@@ -1,6 +1,8 @@
 # == Class: role::pagetriage
 # The extension that powers the New Page Patrol workflow
 class role::pagetriage {
+    require ::role::ores
+
     mediawiki::extension { 'PageTriage':
         needs_update => true,
     }
