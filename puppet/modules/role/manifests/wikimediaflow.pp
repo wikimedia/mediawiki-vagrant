8c58db47a41be2c916109d966fc34d653047d598
Stephane Bisson <sbisson@wikimedia.org>
2018-09-18 08:57:53 -0400

    Remove artifacts from the defunct Flow browser tests
    Change-Id: Ie19cc3aad6615ef2213452e7d0b78155d158eaa1

--- puppet/modules/role/manifests/wikimediaflow.pp
+++ puppet/modules/role/manifests/wikimediaflow.pp
@@ -14,25 +14,6 @@ class role::wikimediaflow {
     include ::role::varnish
     include ::role::visualeditor
 
-    $privileged_username = 'Selenium Flow user'
-    mediawiki::user { $privileged_username:
-        password => $::mediawiki::admin_pass,
-        wiki     => $::mediawiki::db_name,
-        groups   => [
-            'sysop',
-            'suppress',
-            'flow-creator',
-        ],
-    }
-
-    $regular_username = 'Selenium Flow user 2'
-    mediawiki::user { $regular_username:
-        password => $::mediawiki::admin_pass,
-        wiki     => $::mediawiki::db_name,
-    }
-
-    role::centralauth::migrate_user { [ $privileged_username, $regular_username ]: }
-
     mediawiki::settings { 'Wikimedia Flow':
         values   => [
             '$wgDefaultUserOptions[\'flow-editor\'] = \'visualeditor\';',
