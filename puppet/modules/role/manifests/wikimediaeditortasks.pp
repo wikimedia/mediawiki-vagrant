a86ef165467abf7136269868a42b722390addccb
Michael Holloway <mholloway@wikimedia.org>
2019-02-25 21:32:17 -0500

    Add WikimediaEditorTasks role
    Bug: T215091
    Change-Id: Icb5996b70be72996865ffa5175835a90d7b8195b

---
+++ puppet/modules/role/manifests/wikimediaeditortasks.pp
@@ -0,0 +1,14 @@
+# == Class: role::wikimediaeditortasks
+# Installs the WikimediaEditorTasks extension.
+#
+# https://www.mediawiki.org/wiki/Extension:WikimediaEditorTasks
+#
+class role::wikimediaeditortasks {
+  mediawiki::extension { 'WikimediaEditorTasks':
+    needs_update => true,
+  }
+
+  mediawiki::import::text { 'VagrantRoleWikimediaEditorTasks':
+    content => template('role/wikimediaeditortasks/VagrantRoleWikimediaEditorTasks.wiki.erb'),
+  }
+}
