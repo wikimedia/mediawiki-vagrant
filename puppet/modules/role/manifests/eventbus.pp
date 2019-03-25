cf4ceaa6b1a9994b2102b516d40d68c42ed43343
Ottomata <aotto@wikimedia.org>
2019-03-25 14:48:14 -0400

    Conditionally include eventgate module; configure api-request for eventgate
    EventGate requires nodejs 10, which is currently incompatible with
    other node mediawiki services, so we opt-in include it.
    
    Bug: T218238
    Change-Id: Ia7a1b7a93e554a73c09aa228c39c3ea68a6388fd

--- puppet/modules/role/manifests/eventbus.pp
+++ puppet/modules/role/manifests/eventbus.pp
@@ -11,33 +11,23 @@ class role::eventbus {
     require ::eventschemas
     include ::changeprop
 
-    # eventgate is intended to replace eventlogging-serivce-eventbus.
-    # Run them side by side for now.
-    require ::eventgate
-
     $eventbus_url = 'http://localhost:8085/v1/events'
-    $eventgate_url = "http://localhost:${::eventgate::port}/v1/events"
+    $eventgate_url = 'http://localhost:8192/v1/events'
+
+    # EventGate will eventually replace eventlogging-serivce-eventbus.
+    # We'd run them both at the same time, but EventGate requires NodeJS >= 10,
+    # which conflicts with other mediawiki services at this time.
+    # Only install EventGate service if this hiera param is set.
+    # NOTE: You'll also need to set npm::node_version: 10 in hiera
+    # to use the eventgate class.
+    $enable_eventgate = hiera('role::eventbus::enable_eventgate', false)
+    if $enable_eventgate {
+        require ::eventgate
+    }
+
     mediawiki::extension { 'EventBus':
         priority => $::load_early,
-        settings => {
-            'wgEventServices' => {
-                'eventbus'  => {
-                    'url' => $eventbus_url,
-                },
-                'eventgate' => {
-                    'url' => $eventgate_url,
-                },
-            },
-            # Configure EventBusRCFeedEngine to produce
-            # to datacenter1.mediawiki.recentchange topic.
-            'wgRCFeeds'       => {
-                'eventbus' => {
-                    'class'            => 'EventBusRCFeedEngine',
-                    'formatter'        => 'EventBusRCFeedFormatter',
-                    'eventServiceName' => 'eventbus',
-                },
-            },
-        },
+        settings => template('role/eventbus/EventBus.php.erb'),
     }
 
     # TODO: This will be deprecated in favor of eventgate service
