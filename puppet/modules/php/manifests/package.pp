594cf32290fba69938ac370b6e7e98e7bc57d345
jgleeson <jgleeson@wikimedia.org>
2019-04-24 11:13:23 +0100

    Revert "Add support for xdebug on PHP 7.2"
    This reverts commit 0789351e461b6a8367ff095d55da9c0b9cf46f8f.
    
    Bug: T220406
    Change-Id: I71d50d760fe9734dfea196a305d7f2d5b75d84fc

--- puppet/modules/php/manifests/package.pp
+++ puppet/modules/php/manifests/package.pp
@@ -25,21 +25,6 @@ class php::package {
         ]
     }
 
-    # Added PEAR back temporarily to bring in PECL to build xdebug for php7.2 until debain release
-    # a suitale php7.2-xdebug package.
-    package { 'php-pear':
-        ensure  => present,
-    }
-
-    # PEAR's Archive_Tar package (which is used to untar PECL packages including xdebug) doesn't work
-    # due to invalid stynax so we fix it post install. Also back up original file in case it's needed
-    # in future.
-    exec { 'fix_pear':
-        command => 'sed -ri.bk \'s/^(\s+)\$v_att_list = & func_get_args\(\);/\1$v_att_list = func_get_args();/\' /usr/share/php/Archive/Tar.php',
-        onlyif  => 'grep -q "$v_att_list = & func_get_args()" /usr/share/php/Archive/Tar.php',
-        require => Package['php-pear']
-    }
-
     env::alternative { 'default_php_to_7.2':
         alternative => 'php',
         target      => '/usr/bin/php7.2',
---
+++ puppet/modules/php/manifests/remote_debug.pp
@@ -0,0 +1,35 @@
+# == Class: php::remote_debug
+# This class enables support for remote debugging of PHP code using
+# Xdebug. Remote debugging allows you to interactively walk through your
+# code as executes. Remote debugging is most useful when used in
+# conjunction with a PHP IDE such as PhpStorm or Emacs (with Geben).
+# The IDE is installed on your machine, not the Vagrant VM.
+#
+#
+# This was formerly a role, but is now enabled on all
+# MediaWiki-Vagrant installations.
+#
+# To use:
+#
+# -- In your IDE, enable "Start Listening for PHP Debug Connections"
+# -- For Firefox, install
+#    https://addons.mozilla.org/en-US/firefox/addon/the-easiest-xdebug
+#    and click "Enable Debug" icon in the Add-on bar
+# -- Set breakpoints
+# -- Navigate to 127.0.0.1:8080/...
+#
+# See https://www.mediawiki.org/wiki/MediaWiki-Vagrant/Advanced_usage#MediaWiki_debugging_using_Xdebug_and_an_IDE_in_your_host
+# for more information.
+class php::remote_debug {
+    package { 'php-xdebug': }
+
+    php::ini { 'remote_debug':
+        settings => {
+            'xdebug.remote_connect_back' => 1,
+            'xdebug.remote_enable'       => 1,
+            'xdebug.max_nesting_level'   => 200,
+            'xdebug.remote_log'          => '/vagrant/logs/xdebug.log',
+        },
+        require  => Package['php-xdebug'],
+    }
+}
