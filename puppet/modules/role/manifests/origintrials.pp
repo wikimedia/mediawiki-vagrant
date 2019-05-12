181b1783b2c0bb0c137086ac3dbdba27a79dbb64
Gilles Dubuc <gilles@wikimedia.org>
2019-05-12 14:49:30 +0200

    Renew origin trial tokens
    Bug: T216597
    Bug: T216594
    Bug: T216499
    Bug: T216598
    Bug: T209572
    Change-Id: I4f937ada7ce6280dc606f42110d151733cd0932d

--- puppet/modules/role/manifests/origintrials.pp
+++ puppet/modules/role/manifests/origintrials.pp
@@ -5,25 +5,25 @@ class role::origintrials {
     mediawiki::settings { 'origintrials':
         values => {
             wgOriginTrials            => [
-                # Priority hints: Chrome 73-74, expires 2019-05-09 (renewable, trial ends 2019-05-29)
+                # Priority hints: Chrome 73-76, expires 2019-06-23 (renewable, trial ends 2019-09-04)
 
-                'Agv0KONT+NWTohnCv2ck2+/VBAtBMqSpzeSq8i5WS0r90AzrfsVSw94pbEvWU76PMmav3Oh1q+In35ed4KiH+w8AAAB1eyJvcmlnaW4iOiJodHRwczovL2Rldi53aWtpLmxvY2FsLndtZnRlc3QubmV0OjQ0MzAiLCJmZWF0dXJlIjoiUHJpb3JpdHlIaW50cyIsImV4cGlyeSI6MTU1NzM4NDYwNSwiaXNTdWJkb21haW4iOnRydWV9',
+                'ApOdp2cC5d/eRBFoDu9d0RUj71QUvXlBJ8ItzyE5bot75FnWQDr+vnOeRRTxNxqooTloH8DA9uCIznmHDeZkXQYAAAB1eyJvcmlnaW4iOiJodHRwczovL2Rldi53aWtpLmxvY2FsLndtZnRlc3QubmV0OjQ0MzAiLCJmZWF0dXJlIjoiUHJpb3JpdHlIaW50cyIsImV4cGlyeSI6MTU2MTI5MzY5OSwiaXNTdWJkb21haW4iOnRydWV9',
 
-                # Element Timing for Images: Chrome 73-76, expires 2019-05-09 (renewable, trial ends 2019-09-04)
+                # Element Timing for Images: Chrome 73-76, expires 2019-06-23 (renewable, trial ends 2019-09-04)
 
-                'AsU5Tm3NMistsEBeBRBZo7Aw7tX4wvx0pnvQuXdR4rpKqfiQls4uEUeqssCAFh3u8nsmJOpghcRiLXskErB3kQgAAAB7eyJvcmlnaW4iOiJodHRwczovL2Rldi53aWtpLmxvY2FsLndtZnRlc3QubmV0OjQ0MzAiLCJmZWF0dXJlIjoiRWxlbWVudFRpbWluZ0ltYWdlcyIsImV4cGlyeSI6MTU1NzM4NDk4OSwiaXNTdWJkb21haW4iOnRydWV9',
+                'Al+C0/IkhHWWdx+Ibmms2HSVNDMo35sEa1H8cN1tK0PG1FMbLSOw77qTBweenVCvFIrB1rnOfvMYUjSsosSqOgMAAAB7eyJvcmlnaW4iOiJodHRwczovL2Rldi53aWtpLmxvY2FsLndtZnRlc3QubmV0OjQ0MzAiLCJmZWF0dXJlIjoiRWxlbWVudFRpbWluZ0ltYWdlcyIsImV4cGlyeSI6MTU2MTI5MzMzNSwiaXNTdWJkb21haW4iOnRydWV9',
 
-                # Event Timing: Chrome 68-74, expires 2019-05-09 (renewable, trial ends 2019-05-29)
+                # Event Timing: Chrome 68-75, expires 2019-06-23 (renewable, trial ends 2019-07-24)
 
-                'AqtgpahQBVtaEaHb3A9YCscb9qcK3csjM+X3p4L2wniQk2RIEhB0WkLf+cpUacr6S+rlVpg+DcQ+yvIczUDkOw8AAABzeyJvcmlnaW4iOiJodHRwczovL2Rldi53aWtpLmxvY2FsLndtZnRlc3QubmV0OjQ0MzAiLCJmZWF0dXJlIjoiRXZlbnRUaW1pbmciLCJleHBpcnkiOjE1NTczODQzMzUsImlzU3ViZG9tYWluIjp0cnVlfQ==',
+                'Av00Dl155RJppzkab8TAvOblf9+lb4rwogba148hyvJ1SZSHOZGPlEMTCyYpO2C4jAajg1XdWRBrjrDilArDEgsAAABzeyJvcmlnaW4iOiJodHRwczovL2Rldi53aWtpLmxvY2FsLndtZnRlc3QubmV0OjQ0MzAiLCJmZWF0dXJlIjoiRXZlbnRUaW1pbmciLCJleHBpcnkiOjE1NjEyOTMzOTksImlzU3ViZG9tYWluIjp0cnVlfQ==',
 
-                # Feature Policy Reporting: Chrome 73-75, expires 2019-05-09 (renewable, trial ends 2019-07-24)
+                # Feature Policy Reporting: Chrome 73-75, expires 2019-06-23 (renewable, trial ends 2019-07-24)
 
-                'AqjZKHUo2M8HsZKfmsBpdLKsO+wwMLL/9hwS4aAdiNlkP7g95eRBBCI4Ax1IMkZT6PQv4HYusQTwphwt6HkxxQ8AAAB+eyJvcmlnaW4iOiJodHRwczovL2Rldi53aWtpLmxvY2FsLndtZnRlc3QubmV0OjQ0MzAiLCJmZWF0dXJlIjoiRmVhdHVyZVBvbGljeVJlcG9ydGluZyIsImV4cGlyeSI6MTU1NzM4NDkxMSwiaXNTdWJkb21haW4iOnRydWV9',
+                'AqIQhpBKHBEOcxAjNTt5vVGnlcFPS8vvpvpry5xp5HyHEpxguuOp/9r1qLFasuKDcmUHJMu+Vzm/HAE+mYUxTQsAAAB+eyJvcmlnaW4iOiJodHRwczovL2Rldi53aWtpLmxvY2FsLndtZnRlc3QubmV0OjQ0MzAiLCJmZWF0dXJlIjoiRmVhdHVyZVBvbGljeVJlcG9ydGluZyIsImV4cGlyeSI6MTU2MTI5MzQ0NywiaXNTdWJkb21haW4iOnRydWV9',
 
-                # Layout Stability API: Chrome 73-76, expires 2019-05-09 (renewable, trial ends 2019-09-04)
+                # Layout Stability API: Chrome 73-76, expires 2019-06-23 (renewable, trial ends 2019-09-04)
 
-                'AusYYaNce2fQGXeV+V5qol8JMZEDpSkQjABLnYYX6pgOtgR0ofTZBdtMnKPLyCwtmLia6CkB1Qy8G54mOGemxg4AAAB1eyJvcmlnaW4iOiJodHRwczovL2Rldi53aWtpLmxvY2FsLndtZnRlc3QubmV0OjQ0MzAiLCJmZWF0dXJlIjoiTGF5b3V0SmFua0FQSSIsImV4cGlyeSI6MTU1NzQwOTE5OSwiaXNTdWJkb21haW4iOnRydWV9'
+                'ArjJjTh9z2qGTD2oyBTIwkh8m9p+KKszZD3oxvZiCUitIjNiw2CE+XENg4pNPaGFbyKSlKIZ67QyCuMPYDWloAMAAAB1eyJvcmlnaW4iOiJodHRwczovL2Rldi53aWtpLmxvY2FsLndtZnRlc3QubmV0OjQ0MzAiLCJmZWF0dXJlIjoiTGF5b3V0SmFua0FQSSIsImV4cGlyeSI6MTU2MTI5MzYyNiwiaXNTdWJkb21haW4iOnRydWV9'
             ],
             wgReportToExpiry          => 60,
             wgReportToEndpoints       => [
