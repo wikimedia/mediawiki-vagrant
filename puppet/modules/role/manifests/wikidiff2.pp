6cc847f63b9a929629fb67b601d01625f5c67860
Adam Wight <adam.wight@wikimedia.de>
2019-05-22 14:51:28 +0200

    Remove deprecated wikidiff2 configuration
    Wikidiff2 integration is done entirely through PHP initialization now, and
    the MediaWiki config will throw deprecation errors.
    
    Bug: T210172
    Change-Id: I5a949aa2f4647b8e18326513cd79470932d8f728

--- puppet/modules/role/manifests/wikidiff2.pp
+++ puppet/modules/role/manifests/wikidiff2.pp
@@ -5,12 +5,4 @@
 class role::wikidiff2 {
     include ::mediawiki::extension::wikidiff2
     require_package('php-wikidiff2')
-
-    mediawiki::settings { 'wikidiff2':
-        ensure  => present,
-        require => Package['php-wikidiff2'],
-        values  => {
-            'wgExternalDiffEngine' => 'wikidiff2',
-        },
-    }
 }
