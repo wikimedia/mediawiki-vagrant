9d7d2ab75225cafbb7609987ad3b9afa1fa51b86
Brion Vibber <brion@pobox.com>
2019-06-06 14:13:56 -0700

    Add MW-Vagrant support for MIDI in TimedMediaHandler
    Required for testing transcodes to ogg/mp3 of .midi uploads with
    TimedMediaHandler role (patch in progress, expect to be merged soon).
    Installs fluidsynth and fluid-soundfont-gm and sets up TMH's config
    to use them.
    
    Bug: T135597
    Depends-On: Ida690e9c66daa9a135b4807972f077e6babb2447
    Change-Id: I5a0f56a0043b20a7127c5e37fec326c0e176672a

--- puppet/modules/role/manifests/timedmediahandler.pp
+++ puppet/modules/role/manifests/timedmediahandler.pp
@@ -5,16 +5,22 @@ class role::timedmediahandler {
     include ::role::multimedia
 
     require_package('ffmpeg')
+    require_package('fluidsynth')
+    require_package('fluid-soundfont-gm')
 
     mediawiki::extension { 'TimedMediaHandler':
         settings     => {
             wgWaitTimeForTranscodeReset => 1,
-            wgFFmpegLocation            => '/usr/bin/ffmpeg'
+            wgFFmpegLocation            => '/usr/bin/ffmpeg',
+            wgTmhFluidsynthLocation     => '/usr/bin/fluidsynth',
+            wgTmhSoundfontLocation      => '/usr/share/sounds/sf2/FluidR3_GM.sf2'
         },
         needs_update => true,
         composer     => true,
         require      => [
-            Package['ffmpeg']
+            Package['ffmpeg'],
+            Package['fluidsynth'],
+            Package['fluid-soundfont-gm']
         ],
     }
 }
