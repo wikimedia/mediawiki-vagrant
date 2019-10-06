0d1899b58a48d12068e1687c9bd29651bd6564ae
John Erling Blad <jeblad@gmail.com>
2019-10-06 12:41:14 +0200

    Added a Vagrant role for Katsa extension
    This is a simple role, just requiring Scribunto. Adjustments
    for the development environment can be done later, if necessary.
    It als has a wikipage with some utility links for listing on
    the Main_Page. They should be sufficient as a starting point
    for further exploration.
    
    Bug: T234751
    Change-Id: I9333f49461b680940c90cc719cb63502ba04aa22

---
+++ puppet/modules/role/manifests/katsa.pp
@@ -0,0 +1,18 @@
+# == Class: role::katsa
+# The Katsa extension provides functionality to make and run
+# spies for Lua modules, that is the Scribunto extension.
+# Such spies can be complex, yet easily reused.
+class role::katsa {
+    include ::role::scribunto
+
+    mediawiki::extension { 'Katsa':
+        remote   => 'https://github.com/jeblad/Katsa.git',
+        require  => Mediawiki::Extension['Scribunto'],
+        composer => true,
+    }
+
+    mediawiki::import::text { 'VagrantRoleKatsa':
+        source => 'puppet:///modules/role/katsa/VagrantRoleKatsa.wiki',
+    }
+}
+
