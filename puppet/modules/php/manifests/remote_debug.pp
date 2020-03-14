cae02981ff7a2bd3bf67d3ab3228e4b45d334a97
Gergő Tisza <tgr.huwiki@gmail.com>
2020-03-13 17:18:17 -0700

    Move xdebug logfile off the shared directory
    Bug: T247587
    Change-Id: Icf063b542fcec67df41d3a0c43cc7832b0f42738

--- puppet/modules/php/manifests/remote_debug.pp
+++ puppet/modules/php/manifests/remote_debug.pp
@@ -23,12 +23,22 @@
 class php::remote_debug {
     package { 'php-xdebug': }
 
+    # T247587 putting this in a shared folder causes performance issues
+    $logfile = '/var/log/xdebug.log';
+
+    file { $logfile:
+        ensure => present,
+        owner  => $::share_owner,
+        group  => $::share_group,
+        mode   => '0664',
+    }
+
     php::ini { 'remote_debug':
         settings => {
             'xdebug.remote_connect_back' => 1,
             'xdebug.remote_enable'       => 1,
             'xdebug.max_nesting_level'   => 200,
-            'xdebug.remote_log'          => '/vagrant/logs/xdebug.log',
+            'xdebug.remote_log'          => $logfile,
         },
         require  => Package['php-xdebug'],
     }
