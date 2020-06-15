cf5b64d3669e2237eb4890c31cb9132f0f855a06
Gergő Tisza <tgr.huwiki@gmail.com>
2020-06-15 16:07:18 +0200

    Delete logrotate status file when it has errors
    Logrotate messes up its status time over time, which leads to
    logs eating up all space and breaking the box. Logrotate 3.12+,
    coming in Debian Buster, will fix that; until then, just do a
    test rotation periodically and delete the logrotate status file
    on error (which does not cause problems other than making
    low-frequency logs rotate a bit faster than intended).
    
    Bug: T164126
    Change-Id: I5b923929dea6ec955f0d64380099b7567af1eb97

--- puppet/modules/mwv/manifests/packages.pp
+++ puppet/modules/mwv/manifests/packages.pp
@@ -39,4 +39,17 @@ class mwv::packages {
     package { [ 'chef', 'chef-zero' ]:
         ensure => absent,
     }
+
+    # T164126 logrotate messes up its status time over time, which leads to
+    # logs eating up all space and breaking the box. Logrotate 3.12+, coming
+    # in Debian Buster, will fix that; until then, just do a test rotation
+    # periodically and delete the logrotate status file on error (which does
+    # not cause problems other than making low-frequency logs rotate a bit
+    # faster than intended).
+    cron { 'delete_logrotate_status':
+        command => '/usr/sbin/logrotate --debug /etc/logrotate.conf 2> /dev/null || /bin/rm /var/lib/logrotate/status',
+        user    => 'root',
+        # run frequently - the VM is probably not up all day
+        minute  => 0,
+    }
 }
