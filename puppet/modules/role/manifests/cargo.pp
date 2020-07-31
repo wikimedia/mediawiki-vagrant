2fdf07cd5ade162162d84bdc7854d95bae1cca96
Yaron Koren <yaron57@gmail.com>
2020-07-31 18:39:41 +0000

    Add DB settings for Cargo role
    Change-Id: Id38aee9b8d6e015f7d91298c3ef44e03dc5ad323

--- puppet/modules/role/manifests/cargo.pp
+++ puppet/modules/role/manifests/cargo.pp
@@ -1,8 +1,33 @@
 # == Class: role::cargo
 # The Cargo extension allows for storage, querying,
 # display and export of template data.
-class role::cargo {
+class role::cargo (
+    $db_name,
+    $db_user,
+    $db_pass,
+) {
+    # Create the Cargo database
+    mysql::db { $db_name:
+        ensure => present,
+    }
+
+    # Create a user for the Cargo databse
+    mysql::user { $db_user:
+        ensure   => present,
+        grant    => 'CREATE, SELECT, INSERT, UPDATE, DELETE, INDEX, ALTER, DROP ON cargo_db.*',
+        password => $db_pass,
+        require  => Mysql::Db['cargo_db'],
+    }
+
     mediawiki::extension { 'Cargo':
         needs_update => true,
+        settings     => {
+            wgCargoDBtype     => 'mysql',
+            wgCargoDBserver   => 'localhost',
+            wgCargoDBname     => $db_name,
+            wgCargoDBuser     => $db_user,
+            wgCargoDBpassword => $db_pass,
+        },
+        require      => Mysql::User['cargo_user'],
     }
 }
