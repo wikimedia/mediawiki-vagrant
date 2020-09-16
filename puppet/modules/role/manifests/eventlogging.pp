1f8a68b31329b54e61b081c5b2eb882efeb1ded8
Ottomata <aotto@wikimedia.org>
2020-09-16 16:38:06 -0400

    Use eventgate-wikimedia-dev by default
    This removes the default dependency on having Kafka running
    in vagrant to use EventLogging or EventBus.
    
    Also refactor the event service names to not use names
    used in production (to avoid confusion with them).
    
    Bug: T187102
    Bug: T238837
    Change-Id: I01466f6f6df0d2f9a3c7c235d5dda8739e05240b

--- puppet/modules/role/manifests/eventlogging.pp
+++ puppet/modules/role/manifests/eventlogging.pp
@@ -1,25 +1,21 @@
 # == Class: role::eventlogging
 # This role sets up the EventLogging extension for MediaWiki such that
-# events are validated against production schemas but logged locally.
-# There is no backend (eventually the eventgate role will do that),
-# log events can only be inspected in the browser.
+# events are validated against schemas in srv/schemas/event, but logged locally.
+#
+# EventGate will be installed and run, and events will be produced via eventgate.
+#
+# By default, eventgate will output events to the /vagrant/logs/eventgate-events.json
+# log file, or to whatever the eventgate::output parameter is set (in hiera).
+# If eventgate::output == 'kafka', Kafka will be installed and events will
+# be produced to Kafka.
+#
 class role::eventlogging {
     include ::role::syntaxhighlight
-    include ::apache::mod::proxy
-    include ::apache::mod::proxy_http
-    include ::apache::mod::headers
 
-    # Define a 'ELDevServer' parameter for Apache <IfDefine> checks.
-    # This proxies :8080/event.gif to :8100/event.gif.
-    apache::def { 'ELDevServer': }
-
-    # In progress: We are moving away from the EventLogging server backend in favor of eventgate.
-    # Include both eventlogging::devserver and eventgate modules during the migration.
-    # Once production is moved to eventgate, we will remove inclusion eventlogging::devserver.
     # NOTE: Set npm::node_version: 10 in hiera
     $node_version = hiera('npm::node_version', undef)
     if (!$node_version or $node_version < 10) {
-        warning('EventLogging role requires the EventGate service, which requires NodeJS 10. To use it, set npm::node_version: 10 in hieradata/common.yaml (Might break other services.)')
+        warning('EventLogging role requires the EventGate service, which requires NodeJS 10. To use it, vagrant hiera npm::node_version 10 && vagrant provision (Might break other services.)')
     }
 
     # EventLogging will produce to EventGate at wgEventLoggingServiceUri.
