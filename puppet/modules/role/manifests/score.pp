ecc3cc58d9c5f818c082adff026b850c8bc4734a
Reedy <reedy@wikimedia.org>
2020-10-17 17:06:54 +0000

    Score: Use fluidsynth instead of timidity
    Makes it match newer code, and WMF production
    
    Change-Id: I8b65f50a2d0da0ffffcfda50aa47b6ddc4e4ff68

--- puppet/modules/role/manifests/score.pp
+++ puppet/modules/role/manifests/score.pp
@@ -6,14 +6,15 @@
 class role::score {
     include ::role::multimedia
 
-    require_package('lilypond', 'timidity', 'freepats')
+    require_package('lilypond', 'fluidsynth', 'fluid-soundfont-gs', 'fluid-soundfont-gm')
 
     mediawiki::extension { 'Score':
         needs_update => true,
         require      => [
             Package['lilypond'],
-            Package['timidity'],
-            Package['freepats'],
+            Package['fluidsynth'],
+            Package['fluidsynth-soundfont-gs'],
+            Package['fluidsynth-soundfont-gm'],
         ],
     }
 }
