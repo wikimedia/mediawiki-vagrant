122135026712d37e19208cddcfb42b21a5b4018c
Gergő Tisza <tgr.huwiki@gmail.com>
2020-12-20 20:17:58 -0800

    wsoauth role: Fix page name typo
    Change-Id: Iec05220fdab441c4715f7b0357ceb9a9368e3a12

--- puppet/modules/role/manifests/wsoauth.pp
+++ puppet/modules/role/manifests/wsoauth.pp
@@ -26,7 +26,7 @@ class role::wsoauth (
         }
     }
 
-    mediawiki::import::text { 'VagrantRoleOWSOAuth':
+    mediawiki::import::text { 'VagrantRoleWSOAuth':
         source => 'puppet:///modules/role/wsoauth/VagrantRoleWSOAuth.wiki',
     }
 }
