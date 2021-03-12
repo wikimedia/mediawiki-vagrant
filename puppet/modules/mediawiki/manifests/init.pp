1ea8102530e81e6508efc20506b1b6fe7b4c747d
DannyS712 <DannyS712.enwiki@gmail.com>
2021-03-12 15:53:25 +0000

    mediawiki/manifests/init.pp: stop enabling $wgEnableRestAPI
    Deprecated and being removed,
    no need to set to true
    
    Change-Id: I5939bf5a269fc168b2fea5104c9bbe8733d06c61

--- puppet/modules/mediawiki/manifests/init.pp
+++ puppet/modules/mediawiki/manifests/init.pp
@@ -277,11 +277,4 @@ class mediawiki(
         group  => 'root',
         mode   => '0444',
     }
-
-    # Enable the REST API. Only needed while the API is still in development.
-    mediawiki::settings { 'REST API':
-        values => {
-            wgEnableRestAPI => true,
-        },
-    }
 }
