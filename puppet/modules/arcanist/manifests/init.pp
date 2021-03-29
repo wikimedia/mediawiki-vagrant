2b49f7b63e5b881addef8f162074e7cb9d93084b
Gergő Tisza <tgr.huwiki@gmail.com>
2021-03-29 17:47:34 +0200

    Remove libphutil
    According to https://phabricator.wikimedia.org/diffusion/PHUTIL/
    libphutil has been archived and is not needed anymore. Avoid errors
    on gitupdate.
    
    Change-Id: I70c0bfa808b9e01b32a5e5482b7f93fbd0220d21

--- puppet/modules/arcanist/manifests/init.pp
+++ puppet/modules/arcanist/manifests/init.pp
@@ -2,22 +2,15 @@
 
 # == Class: arcanist
 #
-# This class clones arcanist and libphutil for developer use
+# This class clones arcanist for developer use
 #
 # [*deploy_dir*]
-#   The system path to checkout arcanist & libphutil to (example:
-#   '/vagrant/phab').
+#   The system path to checkout arcanist to (example: '/vagrant/phab').
 #
 class arcanist(
     $deploy_dir,
 ){
     include ::php
-
-    git::clone { 'libphutil':
-        directory => "${deploy_dir}/libphutil",
-        branch    => 'wmf/stable',
-        remote    => 'https://phabricator.wikimedia.org/diffusion/PHUTIL/libphutil.git',
-    }
 
     git::clone { 'arcanist':
         directory => "${deploy_dir}/arcanist",
@@ -27,10 +20,6 @@ class arcanist(
 
     env::profile_script { 'add arcanist bin to path':
         content => "export PATH=\$PATH:${deploy_dir}/arcanist/bin",
-    }
-
-    service::gitupdate { 'libphutil':
-        dir => "${deploy_dir}/libphutil",
     }
 
     service::gitupdate { 'arcanist':
