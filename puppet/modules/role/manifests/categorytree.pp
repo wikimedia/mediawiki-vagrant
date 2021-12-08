dae53e59b60aeb572bb4ec95cf062a25aba7455d
James D. Forrester <jforrester@wikimedia.org>
2021-12-08 15:50:37 -0500

    CategoryTree: No need to set wgUseAjax any more, MW ignores it for years
    Change-Id: I21c1aa28f5d69d8efb990e5d7ec24ae32a20ce0e

--- puppet/modules/role/manifests/categorytree.pp
+++ puppet/modules/role/manifests/categorytree.pp
@@ -1,9 +1,8 @@
 # == Class: role::categorytree
 # Configures CategoryTree, an extension for making category trees
+#
+# https://www.mediawiki.org/wiki/Extension:CategoryTree
+#
 class role::categorytree {
-    mediawiki::extension { 'CategoryTree':
-        settings => {
-            wgUseAjax => true,
-        }
-    }
+    mediawiki::extension { 'CategoryTree': }
 }
