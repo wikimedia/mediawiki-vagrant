172463cb96ec2e4604bd4a25bb3e44f452f9d438
Sam Wilson <sam@samwilson.id.au>
2022-09-21 13:34:56 +0800

    Add php-zip package as requirement for DataTransfer
    Change-Id: I74efd665c18d7f865535b9f505951b9ef2ec555f

--- puppet/modules/role/manifests/datatransfer.pp
+++ puppet/modules/role/manifests/datatransfer.pp
@@ -2,6 +2,9 @@
 # The Cargo extension allows for import and export
 # of structured data to and from the wiki.
 class role::datatransfer {
+
+    require_package( 'php-zip' )
+
     mediawiki::extension { 'DataTransfer':
         needs_update => true,
     }
