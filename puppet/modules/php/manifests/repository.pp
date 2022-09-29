546e28f5d4946961bb053a86c90d6f39611f6b00
Matthias Mullie <git@mullie.eu>
2022-09-29 12:38:50 +0200

    Add arm64-compatible repo for php packages
    Meanwhile also added more explicit version number for
    php packages. For php-redis, for example, a wrong version
    ended up being installed.
    
    Change-Id: Ic8e1eaa5e7b37bc5683f44f14f7dcf51b7d4f1d4

--- puppet/modules/php/manifests/repository.pp
+++ puppet/modules/php/manifests/repository.pp
@@ -7,4 +7,13 @@ class php::repository {
         dist       => "${::lsbdistcodename}-wikimedia",
         components => 'component/php74',
     }
+
+    # WMF repo above doesn't serve arm64-compatible PHP packages;
+    # this alternative does
+    apt::repository { 'ondrej-php':
+        uri        => 'https://packages.sury.org/php/',
+        dist       => $::lsbdistcodename,
+        components => 'main',
+        keyfile    => 'puppet:///modules/php/ondrej-php-pubkey.asc',
+    }
 }
