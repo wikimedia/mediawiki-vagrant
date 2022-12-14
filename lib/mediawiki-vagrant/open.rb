3d64fc84146826290ebc44d05e81a2e45d5a3dc3
Gergő Tisza <tgr.huwiki@gmail.com>
2022-12-14 13:17:40 -0800

    vagrant open: Show not implemented warning when not on Linux
    Change-Id: I6b3c7f8c1489e7c199a82a9ef5eca14dea07f6d5

--- lib/mediawiki-vagrant/open.rb
+++ lib/mediawiki-vagrant/open.rb
@@ -21,10 +21,15 @@ module MediaWikiVagrant
     private
 
     def open
+      # Only supports Unix now. Probably Mac could use 'open' instead of
+      # 'xdg-open' and Windows could use 'start' but somebody needs to test it.
+      unless Vagrant::Util::Platform.linux?
+        @env.ui.warn "'vagrant open' is only implemented for Linux at the moment."
+        return 1
+      end
       with_target_vms(nil, single_target: true) do |vm|
         vm.action :up, provision_enabled: true if vm.state.id != :running
         configure do |settings|
-          # shamelessly OS-specific
           fork do
             exec 'xdg-open', "http://dev.wiki.local.wmftest.net:#{settings[:http_port]}",
               out: File::NULL, err: File::NULL
