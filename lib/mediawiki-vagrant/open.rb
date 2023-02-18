71d1ee59cf254b9b7a9b6741bbebf8d994f6c3a8
lectrician1 <jayandseth@gmail.com>
2023-02-18 12:57:20 -0500

    Fix vagrant open command on Windows
    The past implementation called "fork" and then ran the "start" command
    for Windows. However, "fork" does not work on Windows. This change uses
    "spawn" which does work on Windows.
    
    Change-Id: I9a2dfa44c24884fec7ac6dff0c5b702bbed6bc66

--- lib/mediawiki-vagrant/open.rb
+++ lib/mediawiki-vagrant/open.rb
@@ -21,22 +21,26 @@ module MediaWikiVagrant
     private
 
     def open
-      if Vagrant::Util::Platform.linux?
-        open_command = 'xdg-open'
-      elsif Vagrant::Util::Platform.darwin?
-        open_command = 'open'
-      elsif Vagrant::Util::Platform.windows?
-        open_command = 'start'
-      else
-        @env.ui.warn "'vagrant open' is not implemented for your platform."
-        return 1
-      end
       with_target_vms(nil, single_target: true) do |vm|
         vm.action :up, provision_enabled: true if vm.state.id != :running
         configure do |settings|
-          fork do
-            exec open_command, "http://dev.wiki.local.wmftest.net:#{settings[:http_port]}",
+          if Vagrant::Util::Platform.linux?
+            fork do
+              exec 'xdg-open', "http://dev.wiki.local.wmftest.net:#{settings[:http_port]}",
+                out: File::NULL, err: File::NULL
+            end
+          elsif Vagrant::Util::Platform.darwin?
+            fork do
+              exec 'open', "http://dev.wiki.local.wmftest.net:#{settings[:http_port]}",
+                out: File::NULL, err: File::NULL
+            end
+          elsif Vagrant::Util::Platform.windows?
+            # fork doesn't work on Windows, so we use spawn
+            spawn 'start', "http://dev.wiki.local.wmftest.net:#{settings[:http_port]}",
               out: File::NULL, err: File::NULL
+          else
+            @env.ui.warn "'vagrant open' is not implemented for your platform."
+            return 1
           end
         end
       end
