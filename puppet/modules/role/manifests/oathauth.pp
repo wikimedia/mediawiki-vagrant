833370ee1750f835c02ba4b27eb3222772bedb97
Gergő Tisza <tgr.huwiki@gmail.com>
2023-10-22 18:44:27 -0700

    oathauth: Also install WebAuthn
    Change-Id: Ib986f662e352cdbe11e8bd895956f66e60419fdb

--- puppet/modules/role/manifests/oathauth.pp
+++ puppet/modules/role/manifests/oathauth.pp
@@ -1,11 +1,17 @@
 # == Class: role::oathauth
 # Provisions the OATHAuth[https://www.mediawiki.org/wiki/Extension:OATHAuth]
-# extension, which allows two-factor authentication.
+# and WebAuthn[https://www.mediawiki.org/wiki/Extension:WebAuthn] extensions,
+# which allow two-factor authentication.
 #
 class role::oathauth {
     mediawiki::extension { 'OATHAuth':
         needs_update => true,
+        composer     => true,
         settings     => template('role/oathauth/conf.php.erb'),
+    }
+    mediawiki::extension { 'WebAuthn':
+      composer     => true,
+      # TODO configure the relying party once Wikimedia production does it
     }
 }
 
