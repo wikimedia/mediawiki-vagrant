a6c5bcee9de4bdcca7f1b3397b815f0ccfb14a4c
Gergő Tisza <tgr.huwiki@gmail.com>
2023-12-26 23:43:51 -0800

    Add adiutor role
    Change-Id: I52d3aa4b33f083eb8743eacd85b7269b790bd620

---
+++ puppet/modules/role/manifests/adiutor.pp
@@ -0,0 +1,9 @@
+# == Class: role::adiuto
+# Sets up the Adiutor[https://www.mediawiki.org/wiki/Extension:Adiutor]
+# MediaWiki extension, a UI for moderation, triage, and content maintenance
+# tasks.
+#
+class role::adiutor {
+    mediawiki::extension { 'Adiutor':
+    }
+}
