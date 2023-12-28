49a32d709edffbb6e4eee0a2930af1728bfafeff
Gergő Tisza <tgr.huwiki@gmail.com>
2023-12-28 11:10:20 -0800

    adiutor: Typo fix
    Change-Id: I0717a3c77088b0076167b678bea6f9a858906d19

--- puppet/modules/role/manifests/adiutor.pp
+++ puppet/modules/role/manifests/adiutor.pp
@@ -1,4 +1,4 @@
-# == Class: role::adiuto
+# == Class: role::adiutor
 # Sets up the Adiutor[https://www.mediawiki.org/wiki/Extension:Adiutor]
 # MediaWiki extension, a UI for moderation, triage, and content maintenance
 # tasks.
