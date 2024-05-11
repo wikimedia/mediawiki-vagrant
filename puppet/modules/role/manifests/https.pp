3849df7b008648a9beee00610a568beaf2c832a1
Gergő Tisza <tgr.huwiki@gmail.com>
2024-05-11 13:57:40 +0200

    https: Improve cert
    * Use a wildcard certificate which covers all domains used by
      Vagrant. Rename cert file to reflect scope. (This also takes
      care of ensuring the new cert is installed on existing boxes
      which already have the https role enabled.)
    * Install the certificate in the guest OS so that e.g. curl
      does not give warnings.
    * Make the .pem file easily downloadable and show instructions
      on how to add it to the browser's cert store. This is needed
      for testing WebAuthn as navigator.credentials.create() refuses
      to work if there are any TLS errors.
    * Get rid of the hack for modifying openssl.cnf; openssl has
      supported specifying altnames via the command line for quite a
      while. Clean up the hack, when the code is running on an old box.
    * Use heredoc to make the openssl command a little more readable.
    * Reload nginx on cert changes.
    
    Change-Id: I41d7a5cef2609d31e2e40defaa43fc40e346c4c0

--- puppet/modules/role/manifests/https.pp
+++ puppet/modules/role/manifests/https.pp
@@ -5,13 +5,36 @@ class role::https {
         fail("You must configure the HTTPS port to use the 'https' role. (Use 'vagrant config https_port <port>'.)")
     }
 
-    $subject = '/CN=cn.local.wmftest.net/O=MediaWiki-Vagrant/C=US'
-    $keyname = 'devwiki'
+    $subject = '/CN=local.wmftest.net/O=MediaWiki-Vagrant/C=US'
+    $keyname = 'local.wmftest.net'
 
-    exec { 'generate_ssl_key':
-        command => "sed -i 's/^# subjectAltName=email:copy/subjectAltName=DNS:dev.wiki.local.wmftest.net/g' /etc/ssl/openssl.cnf && /usr/bin/openssl req -subj ${subject} -nodes -new -x509 -newkey rsa:2048 -keyout /etc/ssl/certs/${keyname}.key -out /etc/ssl/certs/${keyname}.pem -days 1095",
-        creates => '/etc/ssl/certs/devwiki.pem',
+    # Undo a hack used by an older version of this role
+    exec { 'fix openssl.conf':
+        command => 'sed -i "s/^subjectAltName=DNS:dev.wiki.local.wmftest.net/# subjectAltName=email:copy/g" /etc/ssl/openssl.cnf',
+        onlyif  => 'grep -q "^subjectAltName=DNS:dev.wiki.local.wmftest.net" /etc/ssl/openssl.cnf',
+        before  => Exec['generate ssl key'],
+    }
+    exec { 'generate ssl key':
+        command => @("COMMAND")
+            /usr/bin/openssl req -subj ${subject} -nodes -new -x509 -newkey rsa:2048 \
+                -keyout /etc/ssl/certs/${keyname}.key -out /etc/ssl/certs/${keyname}.pem -days 1095 \
+                -addext 'subjectAltName=DNS:*.local.wmftest.net,DNS:*.wiki.local.wmftest.net'
+            | - COMMAND
+        ,
+        creates => "/etc/ssl/certs/${keyname}.pem",
         before  => Nginx::Site['devwiki'],
+        notify  => Exec['nginx-reload'],
+    }
+    exec { 'add ssl key to OS cert store':
+        command => @("COMMAND")
+            /usr/bin/openssl x509 -outform der \
+                -in /etc/ssl/certs/${keyname}.pem \
+                -out /usr/local/share/ca-certificates/puppet-${keyname}.crt; \
+            update-ca-certificates
+            | - COMMAND
+        ,
+        creates => "/usr/local/share/ca-certificates/puppet-${keyname}.crt",
+        require => Exec['generate ssl key'],
     }
 
     nginx::site { 'devwiki':
@@ -49,5 +72,14 @@ class role::https {
 
         # Load after Mediawiki::Settings['SSL-related settings']
         priority => 20,
+    }
+
+    file { "${::apache::docroot}/${keyname}.pem":
+        ensure  => file,
+        source  => "/etc/ssl/certs/${keyname}.pem",
+        require => Exec['generate ssl key'],
+    }
+    mediawiki::import::text { 'VagrantRoleHttps':
+        content => template('role/https/VagrantRoleHttps.wiki.erb'),
     }
 }
