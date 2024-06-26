e3b7f11c81eef5f9525dd11c5fdadb1a4b2dbd80
Taavi Väänänen <hi@taavi.wtf>
2024-06-26 23:34:42 +0300

    uwsgi: Drop module
    No longer used as Striker and Horizon code has been removed.
    
    Change-Id: Id3f83f2902c06ce18633642d2ca18583acd0e8c0

--- puppet/modules/apache/manifests/mod/uwsgi.pp
+++
@@ -1,8 +0,0 @@
-# == Class: apache::mod::uwsgi
-#
-class apache::mod::uwsgi {
-    package { 'libapache2-mod-uwsgi': }
-    apache::mod_conf { 'uwsgi':
-        require => Package['libapache2-mod-uwsgi'],
-    }
-}
