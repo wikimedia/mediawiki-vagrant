d818e05eb9b7371dfc029273b593ee7650f73c72
Taavi Väänänen <hi@taavi.wtf>
2024-06-26 23:34:06 +0300

    Drop Keystone role
    This was used by Horizon and Striker roles which have just been removed.
    
    Change-Id: I95e1856665e95ebf6f3fe6310ba7f257bf4bb138

--- puppet/modules/apache/manifests/mod/wsgi.pp
+++
@@ -1,8 +0,0 @@
-# == Class: apache::mod::wsgi
-#
-class apache::mod::wsgi {
-    package { 'libapache2-mod-wsgi': }
-    apache::mod_conf { 'wsgi':
-        require => Package['libapache2-mod-wsgi'],
-    }
-}
