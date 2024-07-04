dde74129a6b598b4397c70ff5d7febbed9f63f14
Pppery <mapreader@olum.org>
2024-07-04 16:27:16 -0400

    Drop iegreview role
    Bug: T351889
    Change-Id: I8a338e8b0a1df2127b7e648b63a850eef441b838

--- puppet/modules/role/manifests/iegreview.pp
+++
@@ -1,9 +0,0 @@
-# == Class: role::iegreview
-# Provisions the IEG grant review application.
-#
-# *Note*: The application is provisioned using an Apache named virtual host.
-# Once the role is enabled and provisioned use the URL
-# http://iegreview.local.wmftest.net:8080/ to access the site.
-class role::iegreview {
-    include ::iegreview
-}
