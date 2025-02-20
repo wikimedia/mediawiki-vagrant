335ef48701627d947c3557efa7922b43055cbc5e
Gergő Tisza <tgr.huwiki@gmail.com>
2025-02-20 21:51:54 +0100

    centralauth: Add config for managing banners locally
    ...plus EventLogging which seems to be a hard dependency.
    
    Change-Id: I8d95a0fd7649f650f9de955ed2d37e951a808366

--- puppet/modules/role/manifests/centralnotice.pp
+++ puppet/modules/role/manifests/centralnotice.pp
@@ -3,9 +3,18 @@
 # Add the MediaWiki extensions needed for developing banner delivery tools.
 #
 class role::centralnotice {
+    include ::role::eventlogging
     include ::role::translate
 
     mediawiki::extension { 'CentralNotice':
         needs_update => true,
+        settings     => {
+            wgNoticeInfrastructure            => true,
+            wgNoticeProjects                  => [ 'centralnoticeproject' ],
+            wgNoticeProject                   => 'centralnoticeproject',
+            wgCentralHost                     => 'localhost',
+            wgCentralSelectedBannerDispatcher => "${::mediawiki::server_url}/w/index.php?title=Special:BannerLoader",
+            wgCentralDBname                   => 'wiki',
+        },
     }
 }
